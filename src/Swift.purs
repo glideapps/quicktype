6c2b240f58a5496eff3e5848f6f40814c5a84248
David Siegel <dasiege@microsoft.com>
2017-07-13 14:22:20 -0700

    Doc is a pretty-printing monad

--- src/Swift.purs
+++ src/Swift.purs
@@ -2,8 +2,10 @@ module Swift where
 
 import Prelude
 
-import Data.Foldable (foldMap)
+import Data.Foldable (for_)
 import Data.Maybe (Maybe(..), fromMaybe)
+
+import Doc (Doc, indent, line, string, words)
 
 data SType =
     Struct
@@ -15,20 +17,35 @@ data TPrim = TString | TInt | TArray TPrim | TExtern String
 
 type TValue = { name :: String, typ :: TPrim }
 
-render :: SType -> String
-render (Struct { name, fields }) =
-    """struct """ <> fromMaybe "MyStruct" name <> """
-    {
-    """ <>
-    foldMap renderValue fields <>
-    """
-    }"""
+renderSwift :: SType -> Doc Unit
+renderSwift (Struct { name, fields }) = do
+    line $ words ["struct", fromMaybe "MyStruct" name]
+    line "{"
+    indent do
+        for_ fields renderValue
+    line "}"
 
-renderValue :: TValue -> String
-renderValue { name, typ } = "let " <> name <> ": " <> renderTPrim typ
+renderValue :: TValue -> Doc Unit
+renderValue { name, typ } = line do
+    words ["let", name <> ":"]
+    string " "
+    renderTPrim typ
 
-renderTPrim :: TPrim -> String
-renderTPrim TString = "string"
-renderTPrim TInt = "int"
-renderTPrim (TArray typ) = "[" <> renderTPrim typ <> "]"
-renderTPrim (TExtern name) = name
+renderTPrim :: TPrim -> Doc Unit
+renderTPrim TString = string "string"
+renderTPrim TInt = string "int"
+renderTPrim (TArray typ) =  do
+    string "["
+    renderTPrim typ
+    string "]"
+renderTPrim (TExtern name) = string name
+
+sample :: SType
+sample = Struct
+    { name: Just "Person"
+    , fields:
+        [ { name: "name", typ: TString }
+        , { name: "age", typ: TInt }
+        , { name: "friends", typ: TArray TString }
+        ]
+    }
