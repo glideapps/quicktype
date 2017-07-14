2b5c328d6f26c2f7e43419b1933c5dc2d53d91a1
David Siegel <dasiege@microsoft.com>
2017-07-14 13:29:50 -0700

    Render IRUnion as enum in Swift
    Original commit ad513d9157692eb59e5618f29f0d5888043df5e6

--- src/Swift.purs
+++ src/Swift.purs
@@ -2,19 +2,22 @@ module Swift
     ( renderSwiftClass
     ) where
 
+import Prelude
+
 import Doc
 import IR
-import Prelude
 
 import Data.Foldable (for_, intercalate)
 import Data.Function (on)
-import Data.List (List(..), groupBy)
+
 import Data.List as L
 import Data.List.NonEmpty as NE
-import Data.Tuple (Tuple(..))
+
 import Data.Map as M
 
 import Data.Set as S
+import Data.Tuple (Tuple(..))
+
 
 renderSwiftClass :: IRClassData -> Doc Unit
 renderSwiftClass { name, properties } = do
@@ -25,6 +28,10 @@ renderSwiftClass { name, properties } = do
         let propGroups = L.groupBy (eq `on` _.typ) props
         for_ propGroups renderPropGroup
     line "}"
+    
+    blank
+
+    for_ (M.values properties) renderUnions
 
 renderPropGroup :: NE.NonEmptyList { name :: String, typ :: IRType} -> Doc Unit
 renderPropGroup props = line do
@@ -45,4 +52,35 @@ renderType = case _ of
     IRString -> "String"
     IRArray a -> "[" <> renderType a <> "]"
     IRClass { name } -> name
-    IRUnion s ->  "Either<" <> intercalate ", " (S.map renderType s) <> ">"
+    IRUnion s ->  unionName s
+
+renderUnions :: IRType -> Doc Unit
+renderUnions = case _ of
+    IRUnion s -> do
+        renderUnion s
+        for_ (S.toUnfoldable s :: Array _) renderUnions 
+    IRArray (IRUnion s) -> renderUnion s
+    _ -> pure unit
+
+unionName :: S.Set IRType -> String
+unionName s = intercalate "Or" $ (caseName <$> S.toUnfoldable s :: Array _)
+
+caseName :: IRType -> String
+caseName = case _ of
+    IRArray a -> renderType a <> "s"
+    IRUnion _ ->  "Union"
+    t -> renderType t
+
+renderUnion :: S.Set IRType -> Doc Unit
+renderUnion types = do
+    line $ words ["enum", unionName types, "{"]
+    indent do
+        for_ types \typ -> line do
+            string "case ."
+            string $ "some" <> caseName typ
+            string "("
+            string $ renderType typ
+            string ")"
+    line "}"
+    blank
+
