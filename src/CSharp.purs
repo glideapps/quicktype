d5e8510f5fed371da408b640b199d66e25a85578
Mark Probst <mark.probst@gmail.com>
2017-07-14 12:03:51 -0700

    Unify types
    Original commit b6cf61194a8c5a7947d0bce30c3afa348d5efab1

--- src/CSharp.purs
+++ src/CSharp.purs
@@ -8,6 +8,10 @@ import IR
 import Doc
 
 import Data.Foldable (for_)
+import Data.Tuple as Tuple
+import Data.Map as Map
+import Data.String as String
+import Data.Array as Array
 
 renderTypeToCSharp :: IRType -> String
 renderTypeToCSharp = case _ of
@@ -19,15 +23,15 @@ renderTypeToCSharp = case _ of
     IRString -> "string"
     IRArray a -> renderTypeToCSharp a <> "[]"
     IRClass { name } -> name
-    IRUnion _ -> "FIXME"
+    IRUnion types -> "Either<" <> String.joinWith ", " (map renderTypeToCSharp (Array.fromFoldable types)) <> ">"
 
 renderCSharpClass :: IRClassData -> Doc Unit
 renderCSharpClass { name, properties } = do
     line $ words ["class", name]
     line "{"
     indent do
-        for_ properties \p -> line do
+        for_ (Map.toUnfoldable properties :: Array _) \(Tuple.Tuple pname ptype) -> line do
             string "public "
-            string $ renderTypeToCSharp p.typ
-            words ["", p.name, "{ get; set; }"]
+            string $ renderTypeToCSharp ptype
+            words ["", pname, "{ get; set; }"]
     line "}"
--- src/Main.purs
+++ src/Main.purs
@@ -6,27 +6,83 @@ import Control.Plus (empty)
 import Data.Argonaut.Core (Json, foldJson)
 import Data.Argonaut.Parser (jsonParser)
 import Data.Either (Either)
+import Data.Foldable (find)
+import Data.Maybe (Maybe(..))
 
 import Data.List ((:))
 import Data.List as L
+import Data.Tuple as Tuple
 import Data.List.Types (List(..))
 import Data.StrMap as StrMap
+import Data.Map as Map
 import Data.Foldable (for_)
+import Data.Set as S
 
 import IR
 
 import Doc (Doc())
 import Doc as Doc
 
-import Swift as Swift
+--import Swift as Swift
 import CSharp as CSharp
 
 type Renderer = IRClassData -> Doc Unit
 
 renderers = {
-    csharp: CSharp.renderCSharpClass,
-    swift: Swift.renderSwiftClass
+    csharp: CSharp.renderCSharpClass
+--    swift: Swift.renderSwiftClass
 }
+
+unifyClasses :: IRClassData -> IRClassData -> IRClassData
+unifyClasses { name: na, properties: pa } { name: nb, properties: pb } =
+    { name: na, properties: Map.unionWith unifyTypes pa pb }
+
+removeElement :: forall a. Ord a => (a -> Boolean) -> S.Set a -> { element :: Maybe a, rest :: S.Set a }
+removeElement p s =
+    let element = (find p s)
+    in
+        case element of
+            Just x -> { element: element, rest: S.difference s (S.delete x s) }
+            Nothing -> { element: element, rest: s }
+
+isArray :: IRType -> Boolean
+isArray (IRArray _) = true
+isArray _ = false
+
+isClass :: IRType -> Boolean
+isClass (IRClass _) = true
+isClass _ = false
+
+unifyMaybes :: Maybe IRType -> Maybe IRType -> IRType
+unifyMaybes Nothing Nothing = IRNothing
+unifyMaybes (Just a) Nothing = a
+unifyMaybes Nothing (Just b) = b
+unifyMaybes (Just a) (Just b) = unifyTypes a b
+
+setFromType :: IRType -> S.Set IRType
+setFromType IRNothing = S.empty
+setFromType x = S.singleton x
+
+unifyUnion :: S.Set IRType -> S.Set IRType -> S.Set IRType
+unifyUnion sa sb =
+    let { element: arrayA, rest: sa } = removeElement isArray sa
+        { element: arrayB, rest: sb } = removeElement isArray sb
+        { element: classA, rest: sa } = removeElement isClass sa
+        { element: classB, rest: sb } = removeElement isClass sb
+        unifiedArray = setFromType $ unifyMaybes arrayA arrayB
+        unifiedClasses = setFromType $ unifyMaybes classA classB
+    in
+        S.unions [sa, sb, unifiedArray, unifiedClasses]
+
+unifyTypes :: IRType -> IRType -> IRType
+unifyTypes IRNothing x = x
+unifyTypes x IRNothing = x
+unifyTypes (IRArray a) (IRArray b) = IRArray (unifyTypes a b)
+unifyTypes (IRClass a) (IRClass b) = IRClass (unifyClasses a b)
+unifyTypes (IRUnion a) (IRUnion b) = IRUnion (unifyUnion a b)
+unifyTypes (IRUnion a) b = IRUnion (unifyUnion a (S.singleton b))
+unifyTypes a (IRUnion b) = IRUnion (unifyUnion (S.singleton a) b)
+unifyTypes a b = if a == b then a else IRUnion (S.fromFoldable [a, b])
 
 makeTypeFromJson :: String -> Json -> IRType
 makeTypeFromJson name json = foldJson
@@ -35,21 +91,21 @@ makeTypeFromJson name json = foldJson
     (const IRDouble)
     (const IRString)
     -- Convert from Array to List before we match to make things tidier (foldJson is pretty crude)
-    (L.fromFoldable >>> case _ of
-        Nil -> IRArray IRNothing
-        Cons x _ -> IRArray $ makeTypeFromJson (singularize name) x)
+    (\arr -> let singular = singularize name
+        in
+            IRArray (L.foldl (\t j -> unifyTypes t (makeTypeFromJson singular j)) IRNothing arr))
     fromJObject
     json
     where
-        fromJObject obj = IRClass { name, properties: StrMap.foldMap toProperty obj }
-        toProperty name json = L.singleton { name, typ: makeTypeFromJson name json }
+        fromJObject obj = IRClass { name, properties: Map.fromFoldable $ StrMap.foldMap toProperty obj }
+        toProperty name json = L.singleton $ Tuple.Tuple name (makeTypeFromJson name json)
 
 singularize :: String -> String
 singularize s = "OneOf" <> s
 
 gatherClassesFromType :: IRType -> L.List IRClassData
 gatherClassesFromType = case _ of 
-    IRClass cls -> cls : L.concatMap (gatherClassesFromType <<< _.typ) cls.properties
+    IRClass cls -> cls : L.concatMap gatherClassesFromType (Map.values cls.properties)
     IRArray t -> gatherClassesFromType t
     _ -> empty
 
@@ -63,5 +119,5 @@ jsonToCSharp json =
     jsonParser json
     <#> makeTypeFromJson "TopLevel"
     <#> gatherClassesFromType
-    <#> renderClasses renderers.swift
+    <#> renderClasses renderers.csharp
     <#> Doc.render
