95fda013ebeb58ebc7252ec23689ea0b24c15bad
Mark Probst <mark.probst@gmail.com>
2017-07-15 21:55:56 -0700

    Fuck records
    You can't define type classes on records.
    
    
    Original commit c6cfa980e3b59b6031f7a212aced85d588241272

--- src/IR.purs
+++ src/IR.purs
@@ -5,7 +5,7 @@ import Data.List (List())
 import Data.Set (Set)
 import Data.Map (Map)
 
-type IRClassData = { name :: String, properties :: Map String IRType }
+data IRClassData = IRClassData String (Map String IRType)
 
 data IRType
     = IRNothing
@@ -27,7 +27,7 @@ instance eqIRType :: Eq IRType where
     eq IRBool IRBool = true
     eq IRString IRString = true
     eq (IRArray a) (IRArray b) = a == b
-    eq (IRClass { name: na, properties: pa }) (IRClass { name: nb, properties: pb }) = na == nb && pa == pb
+    eq (IRClass a) (IRClass b) = a == b
     eq (IRUnion a) (IRUnion b) = a == b
     eq _ _ = false
 
@@ -53,10 +53,16 @@ instance ordIRType :: Ord IRType where
     compare (IRArray a) (IRArray b) = compare a b
     compare (IRArray _) _ = LT
     compare _ (IRArray _) = GT
-    compare (IRClass { name: na, properties: pa }) (IRClass { name: nb, properties: pb }) =
-        case compare na nb of
-        EQ -> compare pa pb
-        x -> x
+    compare (IRClass a) (IRClass b) = compare a b
     compare (IRClass _) _ = LT
     compare _ (IRClass _) = GT
     compare (IRUnion a) (IRUnion b) = compare a b
+
+instance eqIRClassData :: Eq IRClassData where
+    eq (IRClassData na pa) (IRClassData nb pb) = na == nb && pa == pb
+
+instance ordIRClassData :: Ord IRClassData where
+    compare (IRClassData na pa) (IRClassData nb pb) =
+        case compare na nb of
+        EQ -> compare pa pb
+        x -> x
