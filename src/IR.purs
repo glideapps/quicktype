7082cba9f8013dc62216bb6d341a1291f092f92f
Mark Probst <mark.probst@gmail.com>
2017-07-17 09:26:16 -0700

    New graph IR

--- src/IR.purs
+++ src/IR.purs
@@ -1,11 +1,76 @@
 module IR where
 
+import Control.Monad.State
+import Control.Monad.State.Class
 import Prelude
-import Data.List (List())
-import Data.Set (Set)
-import Data.Map (Map)
 
-data IRClassData = IRClassData String (Map String IRType)
+import Data.Either (Either(..))
+import Data.Foldable (find, all, any)
+import Data.List (List(..), concatMap, length, singleton, (:))
+import Data.List as L
+import Data.Map (Map)
+import Data.Map as M
+import Data.Maybe (Maybe(..), fromJust)
+import Data.Set (Set)
+import Data.Set as S
+import Data.Tuple (Tuple(..))
+import Partial.Unsafe (unsafePartial)
+
+newtype IRGraph = IRGraph { classes :: List (Either IRClassData Int) }
+
+newtype IRClassData = IRClassData { names :: Set String, properties :: Map String IRType }
+
+makeClass :: String -> Map String IRType -> IRClassData
+makeClass name properties =
+    IRClassData { names: S.singleton name, properties }
+
+emptyGraph :: IRGraph
+emptyGraph = IRGraph { classes: L.Nil }
+
+addClass :: IRClassData -> State IRGraph IRType
+addClass classData =
+    do
+        IRGraph { classes } <- get
+        put (IRGraph { classes: (L.snoc classes (Left classData)) })
+        pure $ IRClass (L.length classes)
+
+redirectClass :: Int -> Int -> State IRGraph Unit
+redirectClass from to =
+    do
+        (IRGraph { classes: c1 }) <- get
+        let c2 = unsafePartial $ fromJust $ L.updateAt from (Right to) c1
+        put (IRGraph { classes: c2 })
+
+getClassFromGraph :: IRGraph -> Int -> IRClassData
+getClassFromGraph graph@(IRGraph { classes }) index =
+    case unsafePartial $ fromJust $ L.index classes index of
+    Left cd -> cd
+    Right i -> getClassFromGraph graph i
+
+getClass :: Int -> State IRGraph IRClassData
+getClass index =
+    do
+        graph <- get
+        pure $ getClassFromGraph graph index
+
+combineClasses :: Int -> Int -> IRClassData -> State IRGraph IRType
+combineClasses ia ib combined =
+    do
+        IRGraph { classes } <- get
+        let newIndex = L.length classes
+        t <- addClass combined
+        redirectClass ia newIndex
+        redirectClass ib newIndex
+        pure t
+
+classesInGraph :: IRGraph -> List IRClassData
+classesInGraph (IRGraph { classes }) =
+    L.concatMap mapper classes
+    where
+        mapper =
+            case _ of
+            Left cd -> L.singleton cd
+            Right _ -> L.Nil
 
 data IRType
     = IRNothing
@@ -15,10 +80,193 @@ data IRType
     | IRBool
     | IRString
     | IRArray IRType
-    | IRClass IRClassData
+    | IRClass Int
     | IRUnion (Set IRType)
 
--- TODO finish later! Just a little equality will go a long way for now
+lookupOrDefault :: forall k v. Ord k => v -> k -> Map k v -> v
+lookupOrDefault default key m =
+    case M.lookup key m of
+    Nothing -> default
+    Just x -> x
+
+removeElement :: forall a. Ord a => (a -> Boolean) -> S.Set a -> { element :: Maybe a, rest :: S.Set a }
+removeElement p s =
+    let element = (find p s)
+    in
+        case element of
+            Just x -> { element: element, rest: S.difference s (S.delete x s) }
+            Nothing -> { element: element, rest: s }
+
+mapM :: forall a b. (a -> State IRGraph b) -> List a -> State IRGraph (List b)
+mapM _ L.Nil = pure L.Nil
+mapM f (x : xs) =
+    do
+        ys <- mapM f xs
+        y <- f x
+        pure $ y : ys
+
+-- FIXME: this is ugly and inefficient
+unionWithDefault :: forall k v. Ord k => (v -> v -> State IRGraph v) -> v -> Map k v -> Map k v -> State IRGraph (Map k v)
+unionWithDefault unifier default m1 m2 =
+    let allKeys = L.fromFoldable $ S.union (S.fromFoldable $ M.keys m1) (S.fromFoldable $ M.keys m2)
+        valueFor k = unifier (lookupOrDefault default k m1) (lookupOrDefault default k m2)
+        keyMapper k =
+            do
+                v <- valueFor k
+                pure $ Tuple k v
+    in
+        do
+            kvps <- mapM keyMapper allKeys
+            pure $ M.fromFoldable kvps
+
+isArray :: IRType -> Boolean
+isArray (IRArray _) = true
+isArray _ = false
+
+isClass :: IRType -> Boolean
+isClass (IRClass _) = true
+isClass _ = false
+
+decomposeTypeSet :: S.Set IRType -> { maybeArray :: Maybe IRType, maybeClass :: Maybe IRType, rest :: S.Set IRType }
+decomposeTypeSet s =
+    let { element: maybeArray, rest: rest } = removeElement isArray s
+        { element: maybeClass, rest: rest } = removeElement isClass rest
+    in
+        { maybeArray, maybeClass, rest }
+
+setFromType :: IRType -> S.Set IRType
+setFromType IRNothing = S.empty
+setFromType x = S.singleton x
+
+unifyClasses :: IRClassData -> IRClassData -> State IRGraph IRClassData
+unifyClasses (IRClassData { names: na, properties: pa }) (IRClassData { names: nb, properties: pb }) =
+    do
+        properties <- unionWithDefault unifyTypesWithNull IRNothing pa pb
+        pure $ IRClassData { names: (S.union na nb), properties: properties }
+
+unifyMaybes :: Maybe IRType -> Maybe IRType -> State IRGraph IRType
+unifyMaybes Nothing Nothing = pure IRNothing
+unifyMaybes (Just a) Nothing = pure a
+unifyMaybes Nothing (Just b) = pure b
+unifyMaybes (Just a) (Just b) = unifyTypes a b
+
+unifyUnion :: S.Set IRType -> S.Set IRType -> State IRGraph (S.Set IRType)
+unifyUnion sa sb =
+    do
+        let { maybeArray: arrayA, maybeClass: classA, rest: sa } = decomposeTypeSet sa
+        let { maybeArray: arrayB, maybeClass: classB, rest: sb } = decomposeTypeSet sb
+        unifiedArray <- unifyMaybes arrayA arrayB
+        unifiedClasses <- unifyMaybes classA classB
+        pure $ S.unions [sa, sb, setFromType unifiedArray, setFromType unifiedClasses]
+
+unifyTypes :: IRType -> IRType -> State IRGraph IRType
+unifyTypes IRNothing x = pure x
+unifyTypes x IRNothing = pure x
+unifyTypes (IRArray a) (IRArray b) =
+    do
+        unified <- unifyTypes a b
+        pure $ IRArray unified
+unifyTypes (IRClass ia) (IRClass ib) =
+    do
+        a <- getClass ia
+        b <- getClass ib
+        unified <- unifyClasses a b
+        combineClasses ia ib unified
+unifyTypes (IRUnion a) (IRUnion b) =
+    do
+        unified <- unifyUnion a b 
+        pure $ IRUnion unified
+unifyTypes (IRUnion a) b =
+    do
+        unified <- unifyUnion a (S.singleton b)
+        pure $ IRUnion unified
+unifyTypes a (IRUnion b) =
+    do
+        unified <- unifyUnion (S.singleton a) b
+        pure $ IRUnion unified
+unifyTypes a b =
+    do
+        pure $ if a == b then a else IRUnion (S.fromFoldable [a, b])
+
+nullifyNothing :: IRType -> IRType
+nullifyNothing IRNothing = IRNull
+nullifyNothing x = x
+
+unifyTypesWithNull :: IRType -> IRType -> State IRGraph IRType
+unifyTypesWithNull IRNothing IRNothing = pure IRNothing
+unifyTypesWithNull a b = unifyTypes (nullifyNothing a) (nullifyNothing b)
+
+matchingProperties :: forall v. Eq v => Map String v -> Map String v -> Map String v
+matchingProperties ma mb =
+    M.fromFoldable (L.concatMap getFromB (M.toUnfoldable ma))
+    where
+        getFromB (Tuple k va) =
+            case M.lookup k mb of
+            Just vb -> if va == vb then Tuple k vb : L.Nil else L.Nil
+            Nothing -> L.Nil
+
+propertiesSimilar :: forall v. Eq v => Map String v -> Map String v -> Boolean
+propertiesSimilar pa pb =
+    let aInB = M.size $ matchingProperties pa pb
+        bInA = M.size $ matchingProperties pb pa
+    in
+        (aInB * 4 >= (M.size pa) * 3) && (bInA * 4 >= (M.size pb) * 3)
+
+isMaybeSubtypeOfMaybe :: IRGraph -> Maybe IRType -> Maybe IRType -> Boolean
+isMaybeSubtypeOfMaybe _ Nothing Nothing = true
+isMaybeSubtypeOfMaybe graph (Just a) (Just b) = isSubtypeOf graph a b
+isMaybeSubtypeOfMaybe _ _ _ = false
+
+isSubtypeOf :: IRGraph ->  IRType -> IRType -> Boolean
+isSubtypeOf _ IRNothing _ = true
+isSubtypeOf graph (IRUnion sa) (IRUnion sb) =
+    all (\ta -> any (isSubtypeOf graph ta) sb) sa
+isSubtypeOf graph (IRArray a) (IRArray b) =
+    isSubtypeOf graph a b
+isSubtypeOf graph (IRClass ia) (IRClass ib) =
+    let IRClassData { names: _, properties: pa } = getClassFromGraph graph ia
+        IRClassData { names: _, properties: pb } = getClassFromGraph graph ib
+    in
+        propertiesAreSubset graph pa pb
+isSubtypeOf _ a b = a == b
+
+propertiesAreSubset :: IRGraph -> Map String IRType -> Map String IRType -> Boolean
+propertiesAreSubset graph ma mb =
+    all isInB (M.toUnfoldable ma :: List _)
+    where
+        isInB (Tuple n ta) =
+            case M.lookup n mb of
+            Nothing -> false
+            Just tb -> isSubtypeOf graph ta tb
+
+classesSimilar :: IRGraph -> IRClassData -> IRClassData -> Boolean
+classesSimilar graph (IRClassData { names: _, properties: pa }) (IRClassData { names: _, properties: pb }) =
+    propertiesSimilar pa pb ||
+    propertiesAreSubset graph pa pb ||
+    propertiesAreSubset graph pb pa
+
+-- similarClasses :: L.List IRClassData -> { classes :: Set IRClassData, replacements :: Map IRClassData IRClassData }
+-- similarClasses L.Nil = { classes: S.empty, replacements: M.empty }
+-- similarClasses (thisClass@(IRClassData name properties) : rest) =
+--     let { yes: similar, no: others } = L.partition isSimilar rest
+--         recursiveResult@{ classes, replacements } = similarClasses others
+--     in
+--         if L.length similar == 0 then
+--             recursiveResult
+--         else
+--             let unified = L.foldl unifyClasses thisClass similar
+--                 newReplacements = M.fromFoldable (map (\c -> Tuple c unified) (thisClass : similar))
+--             in
+--                 { classes: S.insert unified classes, replacements: M.union replacements newReplacements  }         
+--     where
+--         isSimilar = classesSimilar thisClass
+
+combineNames :: S.Set String -> String
+combineNames s =
+    case L.fromFoldable s of
+    L.Nil -> "NONAME"
+    n : _ -> n
+
 instance eqIRType :: Eq IRType where
     eq IRNothing IRNothing = true
     eq IRNull IRNull = true
@@ -59,10 +307,10 @@ instance ordIRType :: Ord IRType where
     compare (IRUnion a) (IRUnion b) = compare a b
 
 instance eqIRClassData :: Eq IRClassData where
-    eq (IRClassData na pa) (IRClassData nb pb) = na == nb && pa == pb
+    eq (IRClassData { names: na, properties: pa }) (IRClassData { names: nb, properties: pb }) = na == nb && pa == pb
 
 instance ordIRClassData :: Ord IRClassData where
-    compare (IRClassData na pa) (IRClassData nb pb) =
+    compare (IRClassData { names: na, properties: pa }) (IRClassData { names: nb, properties: pb }) =
         case compare na nb of
         EQ -> compare pa pb
         x -> x
--- src/Main.purs
+++ src/Main.purs
@@ -7,8 +7,11 @@ import Types
 import CSharp (renderer)
 import CSharp as CSharp
 import Control.Plus (empty)
-import Data.Argonaut.Core (Json, foldJson, isString)
+import Control.Monad.State
+import Control.Monad.State.Class
+import Data.Argonaut.Core (JArray, JBoolean, JNull, JNumber, JString, Json, JObject, foldJson, isString)
 import Data.Argonaut.Parser (jsonParser)
+import Data.Array as A
 import Data.Either (Either)
 import Data.Either.Nested (in1)
 import Data.Foldable (find, for_, all, any)
@@ -28,187 +31,60 @@ import Doc (Doc)
 import Doc as Doc
 import Swift as Swift
 
-lookupOrDefault :: forall k v. Ord k => v -> k -> Map.Map k v -> v
-lookupOrDefault default key m =
-    case Map.lookup key m of
-    Nothing -> default
-    Just x -> x
-
--- FIXME: this is ugly and inefficient
-unionWithDefault :: forall k v. Ord k => (v -> v -> v) -> v -> Map.Map k v -> Map.Map k v -> Map.Map k v
-unionWithDefault unifier default m1 m2 =
-    let allKeys = L.fromFoldable $ S.union (S.fromFoldable $ Map.keys m1) (S.fromFoldable $ Map.keys m2)
-        valueFor k = (unifier (lookupOrDefault default k m1) (lookupOrDefault default k m2))
-        kvps = map (\k -> Tuple.Tuple k (valueFor k)) allKeys
-    in
-        Map.fromFoldable kvps
-
-unifyClasses :: IRClassData -> IRClassData -> IRClassData
-unifyClasses (IRClassData na pa) (IRClassData nb pb) =
-    IRClassData na (unionWithDefault unifyTypesWithNull IRNothing pa pb)
-
-removeElement :: forall a. Ord a => (a -> Boolean) -> S.Set a -> { element :: Maybe a, rest :: S.Set a }
-removeElement p s =
-    let element = (find p s)
-    in
-        case element of
-            Just x -> { element: element, rest: S.difference s (S.delete x s) }
-            Nothing -> { element: element, rest: s }
-
-isArray :: IRType -> Boolean
-isArray (IRArray _) = true
-isArray _ = false
-
-isClass :: IRType -> Boolean
-isClass (IRClass _) = true
-isClass _ = false
-
-unifyMaybes :: Maybe IRType -> Maybe IRType -> IRType
-unifyMaybes Nothing Nothing = IRNothing
-unifyMaybes (Just a) Nothing = a
-unifyMaybes Nothing (Just b) = b
-unifyMaybes (Just a) (Just b) = unifyTypes a b
-
-setFromType :: IRType -> S.Set IRType
-setFromType IRNothing = S.empty
-setFromType x = S.singleton x
-
-decomposeTypeSet :: S.Set IRType -> { maybeArray :: Maybe IRType, maybeClass :: Maybe IRType, rest :: S.Set IRType }
-decomposeTypeSet s =
-    let { element: maybeArray, rest: rest } = removeElement isArray s
-        { element: maybeClass, rest: rest } = removeElement isClass rest
-    in
-        { maybeArray, maybeClass, rest }
-
-unifyUnion :: S.Set IRType -> S.Set IRType -> S.Set IRType
-unifyUnion sa sb =
-    let { maybeArray: arrayA, maybeClass: classA, rest: sa } = decomposeTypeSet sa
-        { maybeArray: arrayB, maybeClass: classB, rest: sb } = decomposeTypeSet sb
-        unifiedArray = setFromType $ unifyMaybes arrayA arrayB
-        unifiedClasses = setFromType $ unifyMaybes classA classB
-    in
-        S.unions [sa, sb, unifiedArray, unifiedClasses]
-
-unifyTypes :: IRType -> IRType -> IRType
-unifyTypes IRNothing x = x
-unifyTypes x IRNothing = x
-unifyTypes (IRArray a) (IRArray b) = IRArray (unifyTypes a b)
-unifyTypes (IRClass a) (IRClass b) = IRClass (unifyClasses a b)
-unifyTypes (IRUnion a) (IRUnion b) = IRUnion (unifyUnion a b)
-unifyTypes (IRUnion a) b = IRUnion (unifyUnion a (S.singleton b))
-unifyTypes a (IRUnion b) = IRUnion (unifyUnion (S.singleton a) b)
-unifyTypes a b = if a == b then a else IRUnion (S.fromFoldable [a, b])
-
-nullifyNothing :: IRType -> IRType
-nullifyNothing IRNothing = IRNull
-nullifyNothing x = x
-
-unifyTypesWithNull :: IRType -> IRType -> IRType
-unifyTypesWithNull IRNothing IRNothing = IRNothing
-unifyTypesWithNull a b = unifyTypes (nullifyNothing a) (nullifyNothing b)
-
-makeTypeFromJson :: String -> Json -> IRType
-makeTypeFromJson name json = foldJson
-    (const IRNull)
-    (const IRBool)
-    (const IRDouble)
-    (const IRString)
-    (\arr -> let typeName = singular name
-        in
-            IRArray (L.foldl (\t j -> unifyTypes t (makeTypeFromJson typeName j)) IRNothing arr))
-    fromJObject
+makeTypeFromJson :: String -> Json -> State IRGraph IRType
+makeTypeFromJson name json =
+    foldJson
+    (\_ -> pure IRNull)
+    (\_ -> pure IRBool)
+    (\_ -> pure IRDouble)
+    (\_ -> pure IRString)
+    (\arr ->
+        do
+            let typeName = singular name
+            unified <- A.foldM (unify typeName) IRNothing arr
+            pure $ IRArray unified)
+    (\obj ->
+        do
+            let l1 = StrMap.toUnfoldable obj
+            l2 <- mapM toProperty l1
+            addClass $ IRClassData { names: (S.singleton name), properties: Map.fromFoldable l2 })
     json
     where
-        fromJObject obj = IRClass (IRClassData name (Map.fromFoldable $ StrMap.foldMap toProperty obj))
-        toProperty name json = L.singleton $ Tuple.Tuple name (makeTypeFromJson name json)
+        unify typeName t1 j2 =
+            do
+                t2 <- makeTypeFromJson typeName j2
+                unifyTypes t1 t2
+        toProperty :: Tuple String Json -> State IRGraph (Tuple String IRType)
+        toProperty (Tuple name json) =
+            do
+                t <- makeTypeFromJson name json
+                pure $ Tuple.Tuple name t
 
-gatherClassesFromType :: IRType -> L.List IRClassData
-gatherClassesFromType = case _ of 
-    IRClass cls@(IRClassData _ properties) -> cls : L.concatMap gatherClassesFromType (Map.values properties)
-    IRArray t -> gatherClassesFromType t
-    IRUnion s -> L.concatMap gatherClassesFromType (L.fromFoldable s)
-    _ -> empty
+-- gatherClassesFromType :: IRType -> L.List IRClassData
+-- gatherClassesFromType = case _ of 
+--     IRClass cls@(IRClassData _ properties) -> cls : L.concatMap gatherClassesFromType (Map.values properties)
+--     IRArray t -> gatherClassesFromType t
+--     IRUnion s -> L.concatMap gatherClassesFromType (L.fromFoldable s)
+--     _ -> empty
 
-matchingProperties :: forall v. Eq v => Map.Map String v -> Map.Map String v -> Map.Map String v
-matchingProperties ma mb =
-    Map.fromFoldable (L.concatMap getFromB (Map.toUnfoldable ma))
-    where
-        getFromB (Tuple.Tuple k va) =
-            case Map.lookup k mb of
-            Just vb -> if va == vb then Tuple.Tuple k vb : L.Nil else L.Nil
-            Nothing -> L.Nil
+-- replaceClasses :: (Map.Map IRClassData IRClassData) -> IRType -> IRType
+-- replaceClasses m t@(IRClass c@(IRClassData name properties)) =
+--     case Map.lookup c m of
+--     Nothing -> IRClass (IRClassData name (Map.mapWithKey (\n t -> replaceClasses m t) properties))
+--     Just replacement -> IRClass replacement
+-- replaceClasses m (IRArray t) = IRArray (replaceClasses m t)
+-- replaceClasses m (IRUnion s) = IRUnion (S.map (replaceClasses m) s)
+-- replaceClasses _ t = t
 
-propertiesSimilar :: forall v. Eq v => Map.Map String v -> Map.Map String v -> Boolean
-propertiesSimilar pa pb =
-    let aInB = Map.size $ matchingProperties pa pb
-        bInA = Map.size $ matchingProperties pb pa
-    in
-        (aInB * 4 >= (Map.size pa) * 3) && (bInA * 4 >= (Map.size pb) * 3)
-
-isMaybeSubtypeOfMaybe :: Maybe IRType -> Maybe IRType -> Boolean
-isMaybeSubtypeOfMaybe Nothing Nothing = true
-isMaybeSubtypeOfMaybe (Just a) (Just b) = isSubtypeOf a b
-isMaybeSubtypeOfMaybe _ _ = false
-
-isSubtypeOf :: IRType -> IRType -> Boolean
-isSubtypeOf IRNothing _ = true
-isSubtypeOf (IRUnion sa) (IRUnion sb) =
-    all (\ta -> any (isSubtypeOf ta) sb) sa
-isSubtypeOf (IRArray a) (IRArray b) =
-    isSubtypeOf a b
-isSubtypeOf (IRClass (IRClassData _ ma)) (IRClass (IRClassData _ mb)) =
-    propertiesAreSubset ma mb
-isSubtypeOf a b = a == b
-
-propertiesAreSubset :: Map.Map String IRType -> Map.Map String IRType -> Boolean
-propertiesAreSubset ma mb =
-    all isInB (Map.toUnfoldable ma :: List _)
-    where
-        isInB (Tuple n ta) =
-            case Map.lookup n mb of
-            Nothing -> false
-            Just tb -> isSubtypeOf ta tb
-
-similarClasses :: L.List IRClassData -> { classes :: S.Set IRClassData, replacements :: Map.Map IRClassData IRClassData }
-similarClasses L.Nil = { classes: S.empty, replacements: Map.empty }
-similarClasses (thisClass@(IRClassData name properties) : rest) =
-    let { yes: similar, no: others } = L.partition isSimilar rest
-        recursiveResult@{ classes, replacements } = similarClasses others
-    in
-        if L.length similar == 0 then
-            recursiveResult
-        else
-            let unified = L.foldl unifyClasses thisClass similar
-                newReplacements = Map.fromFoldable (map (\c -> Tuple.Tuple c unified) (thisClass : similar))
-            in
-                { classes: S.insert unified classes, replacements: Map.union replacements newReplacements  }         
-    where
-        isSimilar (IRClassData _ p) =
-            propertiesSimilar p properties ||
-            propertiesAreSubset p properties ||
-            propertiesAreSubset properties p
-
-replaceClasses :: (Map.Map IRClassData IRClassData) -> IRType -> IRType
-replaceClasses m t@(IRClass c@(IRClassData name properties)) =
-    case Map.lookup c m of
-    Nothing -> IRClass (IRClassData name (Map.mapWithKey (\n t -> replaceClasses m t) properties))
-    Just replacement -> IRClass replacement
-replaceClasses m (IRArray t) = IRArray (replaceClasses m t)
-replaceClasses m (IRUnion s) = IRUnion (S.map (replaceClasses m) s)
-replaceClasses _ t = t
-
-replaceSimilarClasses :: IRType -> IRType
-replaceSimilarClasses t =
-    let { classes, replacements } = similarClasses (gatherClassesFromType t)
-    in
-        replaceClasses replacements t
+-- replaceSimilarClasses :: IRType -> IRType
+-- replaceSimilarClasses t =
+--     let { classes, replacements } = similarClasses (gatherClassesFromType t)
+--     in
+--         replaceClasses replacements t
 
 jsonToCSharp :: String -> Either String String
 jsonToCSharp json =
     jsonParser json
-    <#> makeTypeFromJson "TopLevel"
-    <#> replaceSimilarClasses
-    <#> gatherClassesFromType
-    <#> L.nub
-    <#> CSharp.renderer.render
+    <#> (\j -> execState (makeTypeFromJson "TopLevel" j) emptyGraph)
+    <#> (\g -> CSharp.renderer.render g (classesInGraph g))
     <#> Doc.render
