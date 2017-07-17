b4ee7032b1ab48d739d73c4cbb0c012f7e6cc754
David Siegel <djsiegel@gmail.com>
2017-07-17 13:20:43 -0700

    Add loading convenience method

--- src/CSharp.purs
+++ src/CSharp.purs
@@ -60,8 +60,8 @@ renderCSharp classes = do
     line "namespace QuickType"
     line "{"
     blank
-    
     indent do
+        line "using System.Net;"
         line "using Newtonsoft.Json;"
         blank
         for_ classes \cls -> do
@@ -77,6 +77,17 @@ renderCSharpClass (IRClassData name properties) = do
     line $ words ["class", csNameStyle name]
     line "{"
     indent do
+
+        -- TODO don't rely on 'TopLevel'
+        when (name == "TopLevel") do
+            line "public static TopLevel LoadFromUrl(string url)"
+            line "{"
+            indent do
+                line "var json = new WebClient().DownloadString(url);"
+                line "return JsonConvert.DeserializeObject<TopLevel>(json);"
+            line "}"
+            blank
+
         for_ (Map.toUnfoldable properties :: Array _) \(Tuple.Tuple pname ptype) -> do
             line do
                 string "[JsonProperty(\""
