611963c2047fb9fcdfb27ddbc8c36596440157bd
Mark Probst <mark.probst@gmail.com>
2017-07-20 10:32:39 -0700

    Better command line interface
    Original commit a7318f65157d2c6ef81ae0b4a08f95ee61986940

--- bin/quicktype.js
+++ bin/quicktype.js
@@ -8,10 +8,41 @@ function render(json) {
     return Main.renderJson(renderer)(json).value0;
 }
 
-let args = process.argv.slice(2);
-let url = args[0];
-
-shell.exec(`curl -s ${url} 2> /dev/null`, { silent: true }, (code, json, stderr) => {
+function work(json) {
   let out = render(json);
   shell.echo(out);
-});
+}
+
+function usage() {
+  shell.echo("Usage:");
+  shell.echo("    quicktype          reads JSON from stdin");
+  shell.echo("    quicktype URL      fetches JSON from URL");
+}
+
+let args = process.argv.slice(2);
+
+if (args.length == 0) {
+  // https://gist.github.com/kristopherjohnson/5065599
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  let chunks = [];
+  process.stdin.on('data', function (chunk) {
+    chunks.push(chunk);
+  });
+
+  process.stdin.on('end', function () {
+    work(chunks.join(""));
+  });
+} else if (args.length == 1 && args[0] == "--help") {
+  usage();
+} else if (args.length == 1) {
+  let url = args[0];
+
+  shell.exec(`curl -s ${url} 2> /dev/null`, { silent: true }, (code, json, stderr) => {
+    work(json);
+  });
+} else {
+  usage();
+  shell.exit(1);
+}
