0b997fabf63709612d1defe6a691627ba8bb5d0d
David Siegel <dasiege@microsoft.com>
2017-07-20 11:57:17 -0700

    Unify samples with test harness
    Original commit c57818b02fbd5c98d938bbbd57b511cba7f1a765

---
+++ src/Samples.purs
@@ -0,0 +1,14 @@
+module Samples where
+
+samples :: Array String
+samples = [
+    "pokédex.json",
+    "bitcoin-latest-block.json",
+    "bitcoin-unconfirmed-transactions.json",
+    "github-events.json",
+    "us-average-temperatures.json",
+    "list.json",
+    "sabre1.json",
+    "sabre2.json",
+    "identifiers.json"
+  ]
--- test/csharp/Program.cs
+++ test/csharp/Program.cs
@@ -1,20 +1,14 @@
 ﻿using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace test
 {
     class Program
     {
-        static void Main(string[] args) => TestParse(args[0]).Wait();
-
-        static async Task TestParse(string url)
+        static void Main(string[] args)
         {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            var json = await client.GetStringAsync(url);
+            var path = args[0];
+            var json = File.ReadAllText(path);
             var qt = QuickType.TopLevel.FromJson(json);
         }
     }
