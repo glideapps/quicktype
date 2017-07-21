9162cc45cf59aa977fbeccfeb017cd4b429249f6
David Siegel <dasiege@microsoft.com>
2017-07-21 10:45:10 -0700

    Add intercom

--- app/src/index.js
+++ app/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+window.Intercom("boot", {
+  app_id: "lormewsd"
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
