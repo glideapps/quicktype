6e47d66fb30b5c8979619f6bf2b77a0c79a6191b
David Siegel <dasiege@microsoft.com>
2017-07-21 17:28:36 -0700

    Fix warnings

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-import brace from 'brace';
 import AceEditor from 'react-ace';
 import Dropdown from 'react-dropdown';
 
