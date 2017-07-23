83ff30fe712a357d20582b5186e9497c0d216c74
Mark Probst <mark.probst@gmail.com>
2017-07-23 05:49:36 -0700

    Simplify intToHex
    Original commit dd6595920819b59e23fba171a7e831e79ceb1b9e

--- src/Data/String/Util.purs
+++ src/Data/String/Util.purs
@@ -2,13 +2,9 @@ module Data.String.Util where
 
 import Prelude
 
-import Data.Argonaut.Core (toArray)
-import Data.Array (length, replicate)
 import Data.Array as A
-import Data.Char (fromCharCode, toCharCode)
-import Data.Int.Bits (shr, and)
+import Data.Int as Int
 import Data.Maybe (Maybe(..))
-import Data.String (fromCharArray)
 import Data.String as S
 
 foreign import _plural :: String -> String
@@ -31,19 +27,8 @@ camelCase = S.split (S.Pattern "_") >>> map capitalize >>> S.joinWith ""
 
 intToHex :: Int -> Int -> String
 intToHex width number =
-    let arr = toArray number
+    let arr = S.toCharArray $ Int.toStringAs Int.hexadecimal number
         len = A.length arr
         fullArr = if len < width then A.replicate (width - len) '0' <> arr else arr
     in
         S.fromCharArray fullArr
-    where
-        toDigit i =
-            if i < 10 then
-                fromCharCode $ i + toCharCode '0'
-            else
-                fromCharCode $ i - 10 + toCharCode 'a'
-        toArray i =
-            if i == 0 then
-                []
-            else
-                (toArray $ shr i 4) <> [toDigit $ and i 15]
