2b5e4c22a69a254ba39034c131c1e109386048d9
Mark Probst <mark.probst@gmail.com>
2017-07-23 06:08:47 -0700

    Allow giving test.js a directory of JSON files
    Original commit f4c2eaacb39e47d4f400ddd47933e6b8f4eaa410

--- bin/test.js
+++ bin/test.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
+const path = require("path");
 const shell = require("shelljs");
 const Main = require("../output/Main");
 const Samples = require("../output/Samples");
@@ -12,19 +14,53 @@ function exec(s, opts, cb) {
     return result;
 }
 
-function testCSharp() {
+function absolutize(p) {
+    if (path.isAbsolute(p))
+        return p;
+    return path.join(process.cwd(), p);
+}
+
+function testCSharp(samples) {
     shell.cd("test/csharp");
     shell.exec("dotnet restore", { silent: true });
 
-    Samples.samples.forEach((sample) => {
+    samples.forEach((sample) => {
         console.error(`* Building C# code for ${sample}`);
 
-        let path = `../../app/public/sample/json/${sample}`;
-        exec(`node ../../bin/quicktype.js ${path} > QuickType.cs`);
-        exec(`dotnet run ${path}`);
+        var p = sample;
+        if (!path.isAbsolute(p))
+            p = path.join("..", "..", "app", "public", "sample", "json", p);
+        exec(`node ../../bin/quicktype.js "${p}" > QuickType.cs`);
+        exec(`dotnet run "${p}"`);
     });
 
     shell.cd("../..");
 }
 
-testCSharp();
+if (process.argv.length > 3) {
+    console.log("Usage: " + __filename + " [TEST-DIR]");
+    process.exit(1);
+}
+
+if (process.argv.length == 2) {
+    testCSharp(Samples.samples);
+} else {
+    let dir = process.argv[2];
+    fs.readdir(dir, function(err, items) {
+        if (err) {
+            console.log("Error: Could not read directory " + dir);
+            process.exit(1);
+        }
+        let samples = [];
+        for (var i=0; i<items.length; i++) {
+            let name = items[i];
+            if (name.startsWith(".") || !name.endsWith(".json"))
+                continue;
+            var joined = path.join(dir, name);
+            if (!path.isAbsolute(joined))
+                joined = path.join(process.cwd(), joined);
+            samples.push(joined);
+        }
+        testCSharp(samples);
+    });
+}
