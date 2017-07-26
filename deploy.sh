b4af6aad0f997c7d056076208098c7d61ec123d9
David Siegel <djsiegel@gmail.com>
2017-07-26 15:12:41 -0700

    npm goes first

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,12 @@ ls app
 
 case $DEPLOY_COUNT in
   1)
-    # Deploying gcs react app
-    cd app && npm run deploy && cd ..
-    ;;
-  2)
     # Deploying npm
     cd cli # we have to cd here to make npm deploy the CLI correctly
+    ;;
+  2)
+    # Deploying gcs react app
+    cd app && npm run deploy && cd ..
     ;;
   3)
     # Deploying firebase
