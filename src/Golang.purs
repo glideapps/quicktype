27982284607073d0cdc32ba500ce982d2d0b7602
David Siegel <djsiegel@gmail.com>
2017-07-25 20:15:51 -0700

    Tidy

--- src/Golang.purs
+++ src/Golang.purs
@@ -29,7 +29,7 @@ renderer =
         { nameForClass
         , unionName
         , unionPredicate
-        , nextNameToTry: \s -> "Other" <> s
+        , nextName: ("Other" <> _)
         , forbiddenNames: []
         }
     }
