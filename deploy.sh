c5d307bd91615a751e16f9b97ccef244cf3bcf3a
David Siegel <djsiegel@gmail.com>
2017-07-26 15:25:53 -0700

    Firebase only

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,13 @@
 
 export DEPLOY_COUNT=$((DEPLOY_COUNT+1))
 
-echo about to deploy $DEPLOY_COUNT
-pwd
-ls
-ls app
-
 case $DEPLOY_COUNT in
   1)
     # Deploying npm
     cd cli # we have to cd here to make npm deploy the CLI correctly
     ;;
   2)
-    # Deploying gcs react app
+    # Deploying firebase react app
     cd app && npm run deploy && cd ..
-    ;;
-  3)
-    # Deploying firebase
     ;;
 esac
