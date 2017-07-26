afc478cdce0890b2f8e8bf98aac9f889cabed338
David Siegel <djsiegel@gmail.com>
2017-07-26 14:36:58 -0700

    Try to make deploy work

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export DEPLOY_COUNT=$((DEPLOY_COUNT+1))
+
+case $DEPLOY_COUNT in
+  1)
+    # Deploying gcs react app
+    cd app && npm run deploy && cd ..
+    ;;
+  2)
+    # Deploying npm
+    cd cli # we have to cd here to make npm deploy the CLI correctly
+    ;;
+  3)
+    # Deploying firebase
+    ;;
+esac
