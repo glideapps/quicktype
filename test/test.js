f795a1c6bbaad8fad134b6636d67c6c42b148530
Mark Probst <mark.probst@gmail.com>
2017-07-25 22:15:41 -0700

    Test input/output equality for Go

--- test/test.js
+++ test/test.js
@@ -6,6 +6,105 @@ const shell = require("shelljs");
 const Main = require("../output/Main");
 const Samples = require("../output/Samples");
 
+// https://stackoverflow.com/questions/1068834/object-comparison-in-javascript
+function deepEquals(x, y) {
+    var i;
+    var p;
+
+    // remember that NaN === NaN returns false
+    // and isNaN(undefined) returns true
+    if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
+        return true;
+    }
+
+    // Compare primitives and functions.     
+    // Check if both arguments link to the same object.
+    // Especially useful on the step where we compare prototypes
+    if (x === y) {
+        return true;
+    }
+
+    if ((x instanceof String && y instanceof String) || (x instanceof Number && y instanceof Number)) {
+        if (x.toString() !== y.toString()) {
+            console.log("Number or string not equal.");
+            return false;
+        }
+        return true;
+    }
+
+    // At last checking prototypes as good as we can
+    if (!(x instanceof Object && y instanceof Object)) {
+        console.log("One is not an object.")
+        return false;
+    }
+
+    if (x.constructor !== y.constructor) {
+        console.log("Not the same constructor.");
+        return false;
+    }
+
+    if (x.prototype !== y.prototype) {
+        console.log("Not the same prototype.");
+        return false;
+    }
+
+    if (Array.isArray(x)) {
+        if (x.length !== y.length){
+            console.log("Arrays don't have the same length.");
+            return false;
+        }
+        for (i = 0; i < x.length; i++) {
+            if (!deepEquals(x[i], y[i]))
+                return false;
+        }
+        return true;
+    }
+
+    for (p in y) {
+        // We allow properties in y that aren't present in x
+        // so long as they're null.
+        if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) {
+            if (y[p] !== null) {
+                console.log(`Non-null property ${p} is not expected.`);
+                return false;
+            }
+            continue;
+        }
+        if (typeof y[p] !== typeof x[p]) {
+            console.log(`Properties ${p} don't have the same types.`);
+            return false;
+        }
+    }
+    
+    for (p in x) {
+        if (x.hasOwnProperty(p) && !y.hasOwnProperty(p)) {
+            console.log(`Expected property ${p} not found.`);
+            return false;
+        }
+        if (typeof x[p] !== typeof y[p]) {
+            console.log(`Properties ${p} don't have the same types.`);
+            return false;
+        }
+
+        switch (typeof(x[p])) {
+        case 'object':
+            if (!deepEquals(x[p], y[p])) {
+                return false;
+            }
+            break;
+            
+        default:
+            if (x[p] !== y[p]) {
+                console.log(`Non-object properties ${p} are not equal.`)
+                return false;
+            }
+            break;
+        }
+    }
+    
+    return true;
+}
+
 function exec(s, opts, cb) {
     let result = shell.exec(s, opts, cb);
     if (result.code !== 0) {
@@ -20,47 +119,57 @@ function absolutize(p) {
     return path.join(process.cwd(), p);
 }
 
-function testCSharp(samples) {
+function testCSharp(samples, knownFails) {
     shell.cd("test/csharp");
     shell.exec("dotnet restore", { silent: true });
-
+    
     samples.forEach((sample) => {
         console.error(`* Building C# code for ${sample}`);
-
+        
         var p = sample;
         if (!path.isAbsolute(p))
             p = path.join("..", "..", "app", "public", "sample", "json", p);
         exec(`node ../../cli/quicktype.js -o QuickType.cs "${p}"`);
         exec(`dotnet run "${p}"`);
     });
-
+    
     shell.cd("../..");
 }
 
-function testGolang(samples) {
+function testGolang(samples, knownFails) {
     shell.cd("test/golang");
-
+    
     samples.forEach((sample) => {
         console.error(`* Building Go code for ${sample}`);
-
+        
         var p = sample;
         if (!path.isAbsolute(p))
             p = path.join("..", "..", "app", "public", "sample", "json", p);
         exec(`node ../../cli/quicktype.js -o quicktype.go "${p}"`);
-        exec(`go run main.go quicktype.go < "${p}"`, {silent:true});
+        let outputString = exec(`go run main.go quicktype.go < "${p}"`, {silent:true}).stdout;
+        if (knownFails.indexOf(sample) < 0) {
+            let outputJSON = JSON.parse(outputString);
+            let inputJSON = JSON.parse(fs.readFileSync(p));
+            if (!deepEquals(inputJSON, outputJSON)) {
+                console.log("Error: Output is not equivalent to input.");
+                process.exit(1);
+            }
+        } else {
+            console.log("Known to fail - not checking output.");
+        }
     });
-
+    
     shell.cd("../..");
 }
 
-function testAll(samples) {
-    testGolang(samples);
-    testCSharp(samples);
+function testAll(samples, goFails, csFails) {
+    testGolang(samples, goFails);
+    testCSharp(samples, csFails);
 }
 
 function main(sources) {
     if (sources.length == 0) {
-        testAll(Samples.samples)
+        testAll(Samples.samples, ["identifiers.json"], [])
     } else {
         sources.forEach((source) => {
             if (fs.lstatSync(source).isDirectory()) {
@@ -68,9 +177,9 @@ function main(sources) {
                     fs.readdirSync(source)
                         .filter((name) => name.endsWith(".json") && !name.startsWith("."))
                         .map((name) => absolutize(path.join(source, name)));
-                testAll(samples);
+                testAll(samples, [], []);
             } else {
-                testAll([absolutize(arg)]);
+                testAll([absolutize(arg)], [], []);
             }
         });
     }
