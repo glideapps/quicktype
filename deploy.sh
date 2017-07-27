12ed546541ab8d2ebb0233ac8ff037718e75675d
David Siegel <djsiegel@gmail.com>
2017-07-27 12:39:10 -0700

    Deploy to staging

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ export DEPLOY_COUNT=$((DEPLOY_COUNT+1))
 
 case $DEPLOY_COUNT in
   1)
-    # Deploying npm
-    cd cli # we have to cd here to make npm deploy the CLI correctly
-    ;;
-  2)
     # Deploying firebase react app
     cd app && npm run deploy && cd ..
+    ;;
+  2)
+    # Deploying npm
+    cd cli # we have to cd here to make npm deploy the CLI correctly
     ;;
 esac
