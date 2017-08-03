7afedb029ce6b66811840fe2095acd68795fb074
David Siegel <djsiegel@gmail.com>
2017-08-03 13:45:36 -0700

    Add Spotidy Album sample and test
    Original commit 1c61feac6171179dbc83adc89b6eb5973613470e

--- src/Samples.purs
+++ src/Samples.purs
@@ -4,6 +4,7 @@ samples :: Array String
 samples = [
     "getting-started.json",
     "pokedex.json",
+    "spotify-album.json",
     "flight.json",
     "bitcoin-latest-block.json",
     "bitcoin-transactions.json",
