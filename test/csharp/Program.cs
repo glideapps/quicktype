9917423b98aba4ee9fc5babe88d7f7182c0a318c
David Siegel <djsiegel@gmail.com>
2017-08-12 14:52:31 -0500

    Fix C# sample
    Original commit 41e122651a98aa012e2fce5561f0f8baf68a69ae

--- test/csharp/Program.cs
+++ test/csharp/Program.cs
@@ -9,8 +9,8 @@ namespace test
         {
             var path = args[0];
             var json = File.ReadAllText(path);
-            var qt = TopLevel.Convert.FromJson(json);
-            var output = TopLevel.Convert.ToJson(qt);
+            var qt = QuickType.Convert.FromJson(json);
+            var output = QuickType.Convert.ToJson(qt);
             Console.WriteLine("{0}", output);
         }
     }
