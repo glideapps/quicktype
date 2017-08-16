0e60ce3bc750740a00824d1171a8fb9f6256ca49
David Siegel <djsiegel@gmail.com>
2017-08-16 10:48:14 -0400

    Try non-responsive layout

--- app/src/App.js
+++ app/src/App.js
@@ -176,28 +176,24 @@ class App extends Component {
 
   render() {
     return (
-      <main className="mdc-typography mdc-layout-grid">
-        <div className="mdc-layout-grid__inner">
-            <Sidebar
-              className="mdc-layout-grid__cell mdc-layout-grid__cell--span-4"
-              source={this.state.source}
-              onChangeSource={this.sourceEdited}
-              sampleName={this.state.sampleName}
-              onChangeSample={this.changeSampleName} 
-              rendererName={this.state.rendererName}
-              onChangeRenderer={this.changeRendererName}
-              topLevelName={this.state.topLevelName}
-              onChangeTopLevelName={debounce(this.changeTopLevelName, 300)} />
-            <Editor
-              id="output"
-              className="mdc-layout-grid__cell mdc-layout-grid__cell--span-8"
-              lang={this.getRenderer().aceMode}
-              theme="chrome"
-              value={this.state.output}
-              showGutter={true}
-              />
-            <Snackbar ref={(r) => { this.snackbar = r; }} />
-          </div>
+      <main className="mdc-typography">
+          <Sidebar
+            source={this.state.source}
+            onChangeSource={this.sourceEdited}
+            sampleName={this.state.sampleName}
+            onChangeSample={this.changeSampleName} 
+            rendererName={this.state.rendererName}
+            onChangeRenderer={this.changeRendererName}
+            topLevelName={this.state.topLevelName}
+            onChangeTopLevelName={debounce(this.changeTopLevelName, 300)} />
+          <Editor
+            id="output"
+            lang={this.getRenderer().aceMode}
+            theme="chrome"
+            value={this.state.output}
+            showGutter={true}
+            />
+          <Snackbar ref={(r) => { this.snackbar = r; }} />
       </main>
     );
   }
