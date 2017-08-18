d3fec1a0ca6a22981b2dba7d103dc4e77d35db39
David Siegel <djsiegel@gmail.com>
2017-08-17 20:44:26 -0400

    Disable google analytics on localhost

--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,14 @@ import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import browser from "bowser";
+import ReactGA from 'react-ga';
+import * as _ from "lodash";
+
+if (_.startsWith(window.location.host, "localhost")) {
+  window.ga = () => {};
+} else {
+  ReactGA.initialize("UA-102732788-1");
+}
 
 if (browser.mobile || browser.tablet) {
 } else {
