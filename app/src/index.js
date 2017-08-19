f076e149bcecdafb49635e9c63a53b2a1fe02c92
David Siegel <djsiegel@gmail.com>
2017-08-19 17:20:40 -0400

    Dynamic descriptions

--- app/src/index.js
+++ app/src/index.js
@@ -5,20 +5,71 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import browser from "bowser";
 import ReactGA from 'react-ga';
-import * as _ from "lodash";
 
-if (_.startsWith(window.location.host, "localhost")) {
-  window.ga = () => {};
-} else {
-  ReactGA.initialize("UA-102732788-1");
+const metadataForHost = {
+  "localhost": {
+    title: "In development",
+    description: "quicktype in development environment"
+  },
+  "java.quicktype.io": {
+    title: "JSON to Java classes and helper code",
+    description: "Given JSON sample data, quicktype outputs code for working with that data in Java, Swift, Go, TypeScript, and more."
+  },
+  "csharp.quicktype.io": {
+    title: "JSON to C# classes and helper code",
+    description: "Given JSON sample data, quicktype outputs code for working with that data in C#, TypeScript, Go, and more."
+  },
+  "go.quicktype.io": {
+    title: "JSON to Go structs and marshalling helpers",
+    description: "Given JSON sample data, quicktype outputs code for working with that data in Go, TypeScript, C#, Elm, and more."
+  },
+  "elm.quicktype.io": {
+    title: "JSON to Elm types and decoders",
+    description: "Given JSON sample data, quicktype outputs code for working with that data in Elm, TypeScript, Swift, Go, and more."
+  },
+  "ts.quicktype.io": {
+    title: "JSON to TypeScript interfaces and runtime type-checkers",
+    description: "Given JSON sample data, quicktype outputs code for working with that data in TypeScript, C#, Swift, Go, and more."
+  },
+  "swift.quicktype.io": {
+    title: "JSON to Swift types and conversion helpers",
+    description: "Given JSON sample data, quicktype outputs code for working with that data in Swift, Java, Go, and more."
+  }
+};
+
+function setMetadata() {
+  let meta = metadataForHost[window.location.hostname];
+  if (!meta) return;
+
+  let { title, description } = meta;
+  window.document.title = `${title} – quicktype`;
+  window.document.querySelector('meta[name="description"]').setAttribute("content", description);
 }
 
-if (browser.mobile || browser.tablet) {
-} else {
-  window.Intercom("boot", {
-    app_id: "lormewsd"
-  });
+function startAnalytics() {
+  if (window.location.hostname === "localhost") {
+    window.ga = () => {};
+  } else {
+    ReactGA.initialize("UA-102732788-1");
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function startIntercom() {
+  if (browser.mobile || browser.tablet) {
+  } else {
+    window.Intercom("boot", {
+      app_id: "lormewsd"
+    });
+  }
+}
+
+function main() {
+  setMetadata();
+  startAnalytics();
+  startIntercom();
+
+  ReactDOM.render(<App />, document.getElementById('root'));
+  registerServiceWorker();
+}
+
+main();
