b090c70f3afafe577c850c4102a492a5651f096c
David Siegel <dasiege@microsoft.com>
2017-08-25 14:09:33 -0700

    Work around for bug in json-to-ast
    Original commit d6c4c264be7788f30e2a52c91e6dc02690ec1080

--- src/Data/JSON/AST.js
+++ src/Data/JSON/AST.js
@@ -5,7 +5,8 @@ var parse = require('json-to-ast');
 exports._parse = function (result) {
     return function (s) {
         try {
-            var ast = parse(s, { verbose: false });
+            // https://github.com/vtrushin/json-to-ast/issues/15
+            var ast = parse(s, { verbose: true });
             return result.success(ast);
         } catch (e) {
             return result.fail(e.toString());
