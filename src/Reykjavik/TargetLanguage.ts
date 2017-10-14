bcbe7ab935997be48df4c286b4d40fa90f52bba0
Mark Probst <mark.probst@gmail.com>
2017-10-14 10:04:24 +0200

    First-class CLI support for TS renderers

--- src/Reykjavik/TargetLanguage.ts
+++ src/Reykjavik/TargetLanguage.ts
@@ -7,15 +7,25 @@ import { Renderer, RenderResult } from "./Renderer";
 import { OptionDefinition, RendererOption, TypedRendererOption } from "./Options";
 
 export abstract class TargetLanguage {
-    readonly options: List<RendererOption>;
+    readonly displayName: string;
+    readonly names: string[];
+    readonly extension: string;
+    readonly aceMode: string;
+    readonly optionDefinitions: OptionDefinition[];
 
-    constructor(options: RendererOption[]) {
-        this.options = List(options);
+    constructor(
+        displayName: string,
+        names: string[],
+        extension: string,
+        aceMode: string,
+        options: RendererOption[]
+    ) {
+        this.displayName = displayName;
+        this.names = names;
+        this.extension = extension;
+        this.aceMode = aceMode;
+        this.optionDefinitions = options.map((o: RendererOption) => o.definition);
     }
 
     abstract renderGraph(topLevels: Graph, optionValues: { [name: string]: any }): RenderResult;
-
-    get optionDefinitions(): OptionDefinition[] {
-        return this.options.map((o: RendererOption) => o.definition).toArray();
-    }
 }
