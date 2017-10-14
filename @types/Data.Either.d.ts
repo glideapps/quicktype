6ff18249da697a5051259f994f009c310394b220
David Siegel <dasiege@microsoft.com>
2017-10-14 15:12:13 -0700

    Add fromLeft

--- @types/Data.Either.d.ts
+++ @types/Data.Either.d.ts
@@ -3,3 +3,4 @@ export interface Either<L, R> {}
 export function isRight<T, U>(either: Either<T, U>): boolean;
 export function isLeft<T, U>(either: Either<T, U>): boolean;
 export function fromRight<T, U>(partial: any): (either: Either<T, U>) => U;
+export function fromLeft<T, U>(partial: any): (either: Either<T, U>) => T;
