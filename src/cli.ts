e83bf01ee4858a1de435344bd6c50da325ea8cfb
David Siegel <djsiegel@gmail.com>
2017-12-29 00:03:54 -0800

    Fix is-url import error

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,9 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as _ from "lodash";
-import * as isURL from "is-url";
+
+// The typings for this module are screwy
+const isURL = require("is-url");
 
 import {
     Run,
