4a789addb435017b731a029a66287e0b791a496f
David Siegel <dasiege@microsoft.com>
2017-12-31 14:34:07 -0800

    Deploy web app

--- appcenter/appcenter-post-build.sh
+++ appcenter/appcenter-post-build.sh
@@ -2,6 +2,18 @@
 
 cd $APPCENTER_SOURCE_DIRECTORY
 
+###############################
+### Deploy app.quicktype.io ###
+###############################
+
+if [ "$APPCENTER_BRANCH" == "appcenter" ]; then
+    appcenter \
+        build queue \
+        --app quicktype/app.quicktype.io \
+        --branch master \
+        --token $APPCENTER_TOKEN
+fi
+
 ##############################
 ### Deploy Xcode extension ###
 ##############################
