d6617ed35e8589a1906327f177dbe87b26b97499
David Siegel <dasiege@microsoft.com>
2017-12-31 14:17:57 -0800

    Trigger a build of quicktype/quicktype-xcode post-build

--- appcenter/appcenter-post-build.sh
+++ appcenter/appcenter-post-build.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash -e
 
 cd $APPCENTER_SOURCE_DIRECTORY
+
+##############################
+### Deploy Xcode extension ###
+##############################
+
+if [ "$APPCENTER_BRANCH" == "master" ]; then
+    appcenter \
+        build queue \
+        --app quicktype/quicktype-xcode \
+        --branch master \
+        --token $APPCENTER_TOKEN
+fi
