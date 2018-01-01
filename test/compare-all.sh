1d020a0e23a5433fe56a71677cf09d851199e455
Mark Probst <mark.probst@gmail.com>
2018-01-01 11:33:14 -0800

    Scripts for comparing outputs of versions of quicktype

---
+++ test/compare-all.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+DIR1="$1"
+DIR2="$2"
+DIFFDIR="$3"
+
+for fn in `cd "$DIR1" ; find . -type f` ; do
+    fdir=`dirname "$fn"`
+    mkdir -p "$DIFFDIR/$fdir"
+    f1="$DIR1/$fn"
+    f2="$DIR2/$fn"
+    if [ -f "$f2" ] ; then
+        of="$DIFFDIR/$fn.diff"
+        set +e
+        diff -u "$f1" "$f2" >"$of"
+        set -e
+        lines=`cat "$of" | wc -l`
+        echo "$lines $fn"
+    else
+        echo "missing $fn"
+    fi
+done
---
+++ test/run-for-all.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+INPUT_DIR="$__dir/../test/inputs/json"
+OUTPUT_DIR="$1"
+
+cd "$INPUT_DIR"
+for fn in `find . -name '*.json'` ; do
+    echo $fn
+    fdir=`dirname "$fn"`
+    fbase=`basename "$fn" .json`
+    odir="$OUTPUT_DIR/$fdir"
+    #echo $odir
+    mkdir -p "$odir"
+    node "$__dir/../dist/cli.js" --lang types --declare-unions --quiet -o "$odir/$fbase.types" "$fn"
+done
