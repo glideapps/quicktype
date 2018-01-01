f440a46ecbf50b8e374c4d1d6aef46a830756410
David Siegel <djsiegel@gmail.com>
2018-01-01 11:46:25 -0800

    Handle build failure in post-build script
    Fixes #399

--- appcenter/appcenter-post-build.sh
+++ appcenter/appcenter-post-build.sh
@@ -3,6 +3,11 @@
 cd $APPCENTER_SOURCE_DIRECTORY
 source appcenter/slack.sh
 
+if [ "$AGENT_JOBSTATUS" != "Succeeded" ]; then
+    slack_notify_build_failed
+    exit 0
+fi
+
 #####################
 ### Deploy to npm ###
 #####################
