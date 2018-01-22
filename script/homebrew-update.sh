012ebac8db67ae876fc7cea5663431cb3aa444be
David Siegel <dasiege@microsoft.com>
2018-01-21 22:04:01 -0800

    Automate release to Homebrew
    Fixes #438

---
+++ script/homebrew-update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create a GitHub token with public_repo scope: https://github.com/settings/tokens/new
+# Then set it as:
+# HOMEBREW_GITHUB_TOKEN=...
+
+URL=$(npm info quicktype --json | jq -r .dist.tarball)
+SHA=$(curl $URL | shasum -a 256)
+brew bump-formula-pr --strict quicktype --url="$URL --sha256="$SHA"
