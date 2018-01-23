df3dca4f5ffd40fb6881e71a1d13bde341aced78
David Siegel <dasiege@microsoft.com>
2018-01-22 17:55:56 -0800

    Single if statement for master deploy

--- appcenter/appcenter-post-build.sh
+++ appcenter/appcenter-post-build.sh
@@ -8,61 +8,51 @@ if [ "$AGENT_JOBSTATUS" != "Succeeded" ]; then
     exit 0
 fi
 
-#####################
-### Deploy to npm ###
-#####################
-
 if [ "$APPCENTER_BRANCH" == "master" ]; then
-    # For now we assume we're just about to deploy to npm from Travis
-    # but soon we will do all deployment here, once our full test suite
-    # runs in App Center and App Center supports PRs.
+
+    #####################
+    ### Deploy to npm ###
+    #####################
+
     echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
     npm run pub
     
     slack_notify_deployed
-fi
 
-#########################
-### Deploy to VS Code ###
-#########################
+    #########################
+    ### Deploy to VS Code ###
+    #########################
 
-if [ "$APPCENTER_BRANCH" == "master" ]; then
     appcenter \
         build queue \
         --app quicktype/quicktype-vscode \
         --branch master \
         --token $APPCENTER_TOKEN
-fi
 
-###############################
-### Deploy app.quicktype.io ###
-###############################
+    ###############################
+    ### Deploy app.quicktype.io ###
+    ###############################
 
-if [ "$APPCENTER_BRANCH" == "master" ]; then
     appcenter \
         build queue \
         --app quicktype/app.quicktype.io \
         --branch master \
         --token $APPCENTER_TOKEN
-fi
 
-##############################
-### Deploy Xcode extension ###
-##############################
+    ##############################
+    ### Deploy Xcode extension ###
+    ##############################
 
-if [ "$APPCENTER_BRANCH" == "master" ]; then
     appcenter \
         build queue \
         --app quicktype/quicktype-xcode \
         --branch master \
         --token $APPCENTER_TOKEN
-fi
 
-###############################
-### Submit a PR to Homebrew ###
-###############################
+    ###############################
+    ### Submit a PR to Homebrew ###
+    ###############################
 
-if [ "$APPCENTER_BRANCH" == "master" ]; then
     # We only submit PRs when patch version ends in 0
     if [[ `npm show quicktype version` == *0 ]]; then
         script/homebrew-update.sh
