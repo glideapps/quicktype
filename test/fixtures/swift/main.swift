57884cb0297c137e363d229146f7100f1663974f
David Siegel <dvdsgl@users.noreply.github.com>
2018-01-27 18:16:56 -0800

    Allow convenience initializers and serializers to throw (#463)
    * Don't swallow exceptions from JSONDecoder.decode
    * Propagate exceptions when encoding
    
    Fixes #462

--- test/fixtures/swift/main.swift
+++ test/fixtures/swift/main.swift
@@ -9,27 +9,19 @@
 import Foundation
 
 let filename = CommandLine.arguments[1]
-guard let data = FileHandle(forReadingAtPath: filename)?.readDataToEndOfFile()
-else {
+
+guard let data = FileHandle(forReadingAtPath: filename)?.readDataToEndOfFile() else {
     print("Error: Could not read input file")
     exit(1)
 }
 
-guard let obj = TopLevel(data: data)
-else {
+guard let obj = try TopLevel(data: data) else {
     print("Error: Could not deserialize")
     exit(1)
 }
 
-guard let newData = obj.jsonData
-else {
+guard let jsonString = try obj.jsonString() else {
     print("Error: Could not serialize")
-    exit(1)
-}
-
-guard let jsonString = String(data: newData, encoding: String.Encoding.utf8)
-else {
-    print("Error: Could not stringify")
     exit(1)
 }
 
