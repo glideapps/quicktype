0717264bce2cfdab9a55363ac693ad3d064dc9ef
David Siegel <dasiege@microsoft.com>
2018-02-18 13:42:51 -0800

    Test we're on a PR

--- .buildkite/build-pr.sh
+++ .buildkite/build-pr.sh
@@ -1,8 +1,11 @@
-# Build the merge commit of the PR
-git fetch origin +refs/pull/$BUILDKITE_PULL_REQUEST/merge:
-git checkout -qf FETCH_HEAD
+if [ $BUILDKITE_PULL_REQUEST != false ] ; then
+    # Build the merge commit of the PR
+    git fetch origin +refs/pull/$BUILDKITE_PULL_REQUEST/merge:
+    git checkout -qf FETCH_HEAD
+fi
 
 docker system prune --force
+
 docker pull schani/quicktype
 docker build --cache-from schani/quicktype -t quicktype .
 docker run -t --workdir="/app" -e FIXTURE quicktype npm test
