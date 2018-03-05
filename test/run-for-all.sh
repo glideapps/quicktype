6c5a2c74010d406a100f1551a4892268dd0069d2
Mark Probst <mark.probst@gmail.com>
2018-03-05 08:53:47 -0800

    Also run schema samples in the compare-runner script

--- test/run-for-all.sh
+++ test/run-for-all.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-LANGUAGE="types"
+LANGUAGE="swift"
+OPTIONS="--just-types"
 
 set -o errexit
 set -o nounset
@@ -8,16 +9,27 @@ set -o pipefail
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-INPUT_DIR="$__dir/../test/inputs/json"
+SCHEMA_INPUT_DIR="$__dir/../test/inputs/schema"
+JSON_INPUT_DIR="$__dir/../test/inputs/json"
+
 OUTPUT_DIR="$1"
 
-cd "$INPUT_DIR"
+cd "$SCHEMA_INPUT_DIR"
+for fn in `find . -name '*.schema'` ; do
+    echo $fn
+    fdir=`dirname "$fn"`
+    fbase=`basename "$fn" .schema`
+    odir="$OUTPUT_DIR/schema/$fdir"
+    mkdir -p "$odir"
+    node "$__dir/../dist/cli.js" --lang "$LANGUAGE" $OPTIONS --src-lang schema --quiet -o "$odir/$fbase.$LANGUAGE" "$fn"
+done
+
+cd "$JSON_INPUT_DIR"
 for fn in `find . -name '*.json'` ; do
     echo $fn
     fdir=`dirname "$fn"`
     fbase=`basename "$fn" .json`
-    odir="$OUTPUT_DIR/$fdir"
-    #echo $odir
+    odir="$OUTPUT_DIR/json/$fdir"
     mkdir -p "$odir"
-    node "$__dir/../dist/cli.js" --lang "$LANGUAGE" --quiet -o "$odir/$fbase.$LANGUAGE" "$fn"
+    node "$__dir/../dist/cli.js" --lang "$LANGUAGE" $OPTIONS --quiet -o "$odir/$fbase.$LANGUAGE" "$fn"
 done
