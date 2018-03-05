0565a7254622e2c403273b99c34bfb7e31abf459
Mark Probst <mark.probst@gmail.com>
2018-03-04 22:39:37 -0800

    Just fetch the branch ourselves goddamn
    GitHub for some reason is super slow in updating the merge branch, so we
    have to roll it ourselves.

--- .buildkite/build-pr.sh
+++ .buildkite/build-pr.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ "x$BUILDKITE_PULL_REQUEST_BASE_BRANCH" != "x" ] ; then
+    git --no-pager fetch origin "pull/$BUILDKITE_PULL_REQUEST/merge:pr"
+    git --no-pager status
+    git --no-pager reset --hard HEAD
+    git --no-pager checkout pr
+    git --no-pager merge master
+fi
+
 git --no-pager log 'HEAD~5..HEAD'
 
 docker system prune --force
