c9a3b74edc92bdc0faf421ac05c4c1a7e8fd97c1
Mark Probst <mark.probst@gmail.com>
2018-03-07 09:48:36 -0800

    Don't assume we have a merge commit, and factor out stuff

--- .buildkite/build-pr.sh
+++ .buildkite/build-pr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-BUILDKITE_COMMIT="`git rev-parse HEAD^`"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$DIR/common.sh"
 
-QUICKTYPE_OUTPUTS="`mktemp -d`"
-git --no-pager show -s --format=fuller HEAD >"$QUICKTYPE_OUTPUTS/merge-commit"
+make_outputs_dir
 
 docker system prune --force
 
@@ -12,25 +12,4 @@ docker pull schani/quicktype
 docker build --cache-from schani/quicktype -t quicktype .
 docker run -t --workdir="/app" -e FIXTURE -v "$QUICKTYPE_OUTPUTS:/quicktype-outputs" -e "OUTPUT_DIR=/quicktype-outputs" quicktype npm test
 
-pushd ..
-aws --output text ssm get-parameters --names buildkite-id-rsa --with-decryption --query 'Parameters[0].Value' >id_rsa
-chmod 600 id_rsa
-
-if [ -d quicktype-outputs ] ; then
-    rm -rf quicktype-outputs
-fi
-GIT_SSH_COMMAND='ssh -i id_rsa' git clone git@github.com:quicktype/quicktype-outputs.git
-cd ./quicktype-outputs
-if [ ! -d outputs ] ; then
-    mkdir outputs
-fi
-COMMIT_DIR="`pwd`/outputs/$BUILDKITE_COMMIT"
-if [ ! -d "$COMMIT_DIR" ] ; then
-    mkdir "$COMMIT_DIR"
-fi
-cp -r "$QUICKTYPE_OUTPUTS"/* "$COMMIT_DIR/"
-git --no-pager add -A
-git --no-pager commit --no-edit -m "Outputs for $BUILDKITE_COMMIT"
-
-GIT_SSH_COMMAND='ssh -i ../id_rsa' git push origin master
-popd
+commit_outputs
