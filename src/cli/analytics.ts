abca16b3269d86b2013b61cf0b7dfd3b216d8e1f
David Siegel <dasiege@microsoft.com>
2018-03-21 14:37:03 -0700

    Add tests for CI detection

--- src/cli/analytics.ts
+++ src/cli/analytics.ts
@@ -1,46 +1,10 @@
 import * as ga from "universal-analytics";
 import * as storage from "./storage";
-
-import { env } from "process";
-import { createHash } from "crypto";
+import * as ci from "./ci";
 
 const uuid = require("uuid/v4");
 
 const GoogleAnalyticsID = "UA-102732788-5";
-
-// A map from CI providers to environment variables that uniquely identify a project.
-// If present, these variables are hashed to produce a unique userId for quicktype in CI.
-const CIProviders: [string, Array<string | undefined>][] = [
-    ["Travis", [env.TRAVIS_REPO_SLUG]],
-    ["Jenkins", [env.JENKINS_URL]],
-    ["TeamCity", [env.TEAMCITY_PROJECT_NAME]],
-    ["CircleCI", [env.CIRCLE_PROJECT_USERNAME, env.CIRCLE_PROJECT_REPONAME]],
-    ["Codeship", [env.CI_REPO_NAME]],
-    ["GitLab", [env.GITLAB_CI, env.CI_REPOSITORY_URL, env.CI_PROJECT_ID]],
-    ["VSTS", [env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI, env.BUILD_REPOSITORY_NAME]],
-    ["App Center", [env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI, env.BUILD_REPOSITORY_NAME]],
-    // We intentionally collapse users with env.CI to prevent over-counting for unidentified CI
-    ["Other", [env.CI]]
-];
-
-function getCIProvider(): { provider: string; userId: string } | undefined {
-    function tryHash(envs: Array<string | undefined>): string | undefined {
-        if (envs.some(s => s === undefined)) {
-            return undefined;
-        }
-        const hash = createHash("sha256");
-        envs.forEach(s => hash.update(s as string));
-        return hash.digest("base64");
-    }
-
-    for (const [provider, vars] of CIProviders) {
-        const hash = tryHash(vars);
-        if (hash !== undefined) {
-            return { provider, userId: hash };
-        }
-    }
-    return undefined;
-}
 
 export interface Analytics {
     pageview(page: string): void;
@@ -66,13 +30,13 @@ export class GoogleAnalytics implements Analytics {
     private readonly visitor: ga.Visitor;
 
     constructor() {
-        const ci = getCIProvider();
-        const userId = ci !== undefined ? ci.userId : storage.get("userId", uuid());
+        const ciInfo = ci.getProvider();
+        const userId = ciInfo !== undefined ? ciInfo.userId : storage.get("userId", uuid());
 
         this.visitor = ga(GoogleAnalyticsID, userId);
 
-        if (ci !== undefined) {
-            this.ciProvider = ci.provider;
+        if (ciInfo !== undefined) {
+            this.ciProvider = ciInfo.provider;
         }
     }
 
