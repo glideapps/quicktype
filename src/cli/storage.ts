b39ebec6a4c9a8c6c8ecfb94cb00dc5a0757f2a3
Mark Probst <mark.probst@gmail.com>
2018-03-21 07:36:54 -0700

    Don't print an error if telemetry is not initialized

--- src/cli/storage.ts
+++ src/cli/storage.ts
@@ -3,6 +3,9 @@ import { homedir } from "os";
 
 import * as persist from "node-persist";
 
+// Users of quicktype as a library shouldn't have to init telemetry.
+let inited: boolean = false;
+
 export async function init() {
     try {
         await persist.init({
@@ -11,22 +14,28 @@ export async function init() {
     } catch (error) {
         console.error(`Could not initialize persistence`, error);
     }
+    inited = true;
 }
+
 export function get<T>(name: string, def: T, onError?: T): T {
-    try {
-        let v = persist.getItemSync(name);
-        if (v === undefined) {
-            set(name, def);
-            v = def;
+    if (inited) {
+        try {
+            let v = persist.getItemSync(name);
+            if (v === undefined) {
+                set(name, def);
+                v = def;
+            }
+            return v;
+        } catch (error) {
+            console.error(`Could not get ${name}`, error);
         }
-        return v;
-    } catch (error) {
-        console.error(`Could not get ${name}`, error);
-        return onError !== undefined ? onError : def;
     }
+    return onError !== undefined ? onError : def;
 }
 
 export function set<T>(name: string, val: T): void {
+    if (!inited) return;
+
     try {
         persist.setItemSync(name, val);
     } catch (error) {
