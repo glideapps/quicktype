c0b1831781908d979132db2676aa2e06f2aa6cc6
David Siegel <dasiege@microsoft.com>
2018-03-21 11:53:19 -0700

    If running on CI, create a stable userId based on the project
    This prevents proliferation of users in environments where ~/.quicktype is not cached

--- src/cli/analytics.ts
+++ src/cli/analytics.ts
@@ -1,9 +1,44 @@
 import * as ga from "universal-analytics";
 import * as storage from "./storage";
 
+import { env } from "process";
+import { createHash } from "crypto";
+
 const uuid = require("uuid/v4");
 
 const GoogleAnalyticsID = "UA-102732788-5";
+
+// A map from CI providers to environment variables that uniquely identify a project.
+// If present, these variables are hashed to produce a unique userId for quicktype in CI.
+const CIProviders: { [provider: string]: Array<string | undefined> } = {
+    travis: [env.TRAVIS_REPO_SLUG],
+    jenkins: [env.JENKINS_URL],
+    teamcity: [env.TEAMCITY_PROJECT_NAME],
+    circleci: [env.CIRCLE_PROJECT_USERNAME, env.CIRCLE_PROJECT_REPONAME],
+    codeship: [env.CI_REPO_NAME],
+    gitlab: [env.GITLAB_CI, env.CI_REPOSITORY_URL, env.CI_PROJECT_ID],
+    vsts: [env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI, env.BUILD_REPOSITORY_NAME],
+    appcenter: [env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI, env.BUILD_REPOSITORY_NAME]
+};
+
+function getCIProvider(): { provider: string; userId: string } | undefined {
+    function tryHash(...envs: Array<string | undefined>): string | undefined {
+        if (envs.some(s => s === undefined)) {
+            return undefined;
+        }
+        const hash = createHash("sha256");
+        envs.forEach(s => hash.update(s as string));
+        return hash.digest("base64");
+    }
+
+    for (const provider of Object.keys(CIProviders)) {
+        const hash = tryHash(...CIProviders[provider]);
+        if (hash !== undefined) {
+            return { provider, userId: hash };
+        }
+    }
+    return undefined;
+}
 
 export interface Analytics {
     pageview(page: string): void;
@@ -29,8 +64,14 @@ export class GoogleAnalytics implements Analytics {
     private readonly visitor: ga.Visitor;
 
     constructor() {
-        const userId = storage.get("userId", uuid());
+        const ci = getCIProvider();
+        const userId = ci !== undefined ? ci.userId : storage.get("userId", uuid());
+
         this.visitor = ga(GoogleAnalyticsID, userId);
+
+        if (ci !== undefined) {
+            this.visitor.set("dimension1", ci.provider);
+        }
     }
 
     pageview(page: string): void {
