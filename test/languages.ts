a4180b3744818d3f5054f7675c3aec0588f5abfb
Mark Probst <mark.probst@gmail.com>
2018-03-31 17:08:59 -0700

    Top-level enum "fix" for Swift

--- test/languages.ts
+++ test/languages.ts
@@ -364,8 +364,10 @@ export const SwiftLanguage: Language = {
   ],
   skipMiscJSON: false,
   skipSchema: [
-    // The top-level is a union
-    "implicit-class-array-union.schema"
+    // The top-level is a union, which Swift's JSON types don't support
+    "implicit-class-array-union.schema",
+    // The code we generate for top-level enums is incompatible with the driver
+    "top-level-enum.schema"
   ],
   rendererOptions: {},
   quickTestRendererOptions: [{ "struct-or-class": "class" }, { density: "dense" }, { density: "normal" }],
