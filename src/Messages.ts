b482458b76c9a3cda4f4afb8f2005935a647b8f7
Mark Probst <mark.probst@gmail.com>
2018-04-04 11:07:39 -0700

    Use our own Error type for exceptions

--- src/Messages.ts
+++ src/Messages.ts
@@ -1,5 +1,7 @@
 "use strict";
 
+import { StringMap } from "./Support";
+
 export enum ErrorMessage {
     InternalError = "Internal error: ${message}",
 
@@ -49,84 +51,93 @@ export enum ErrorMessage {
 
 /*
 type Error =
-    | { message: ErrorMessage.InternalError; props: { message: string } }
+    | { message: ErrorMessage.InternalError; properties: { message: string } }
 
     // Misc
-    | { message: ErrorMessage.JSONParseError; props: { description: string; address: string; message: string } }
-    | { message: ErrorMessage.ReadError; props: { fileOrURL: string; message: string } }
+    | { message: ErrorMessage.JSONParseError; properties: { description: string; address: string; message: string } }
+    | { message: ErrorMessage.ReadError; properties: { fileOrURL: string; message: string } }
 
     // JSON Schema input
-    | { message: ErrorMessage.ArrayIsInvalidJSONSchema; props: {} }
-    | { message: ErrorMessage.NullIsInvalidJSONSchema; props: {} }
-    | { message: ErrorMessage.RefMustBeString; props: {} }
-    | { message: ErrorMessage.AdditionalTypesForbidRequired; props: {} }
-    | { message: ErrorMessage.NoTypeSpecified; props: {} }
-    | { message: ErrorMessage.FalseSchemaNotSupported; props: {} }
-    | { message: ErrorMessage.RefWithFragmentNotAllowed; props: { ref: string } }
-    | { message: ErrorMessage.InvalidJSONSchemaType; props: { type: string } }
-    | { message: ErrorMessage.RequiredMustBeStringOrStringArray; props: { actual: any } }
-    | { message: ErrorMessage.RequiredElementMustBeString; props: { element: any } }
-    | { message: ErrorMessage.TypeMustBeStringOrStringArray; props: { actual: any } }
-    | { message: ErrorMessage.TypeElementMustBeString; props: { element: any } }
-    | { message: ErrorMessage.ArrayItemsMustBeStringOrArray; props: { actual: any } }
-    | { message: ErrorMessage.IDMustHaveAddress, props: { id: string } }
-    | { message: ErrorMessage.WrongAccessorEntryArrayLength, props: { operation: string } }
-    | { message: ErrorMessage.SetOperationCasesIsNotArray; props: { operation: string; cases: any } }
-    | { message: ErrorMessage.CannotFetchSchema; props: { address: string } }
+    | { message: ErrorMessage.ArrayIsInvalidJSONSchema; properties: {} }
+    | { message: ErrorMessage.NullIsInvalidJSONSchema; properties: {} }
+    | { message: ErrorMessage.RefMustBeString; properties: {} }
+    | { message: ErrorMessage.AdditionalTypesForbidRequired; properties: {} }
+    | { message: ErrorMessage.NoTypeSpecified; properties: {} }
+    | { message: ErrorMessage.FalseSchemaNotSupported; properties: {} }
+    | { message: ErrorMessage.RefWithFragmentNotAllowed; properties: { ref: string } }
+    | { message: ErrorMessage.InvalidJSONSchemaType; properties: { type: string } }
+    | { message: ErrorMessage.RequiredMustBeStringOrStringArray; properties: { actual: any } }
+    | { message: ErrorMessage.RequiredElementMustBeString; properties: { element: any } }
+    | { message: ErrorMessage.TypeMustBeStringOrStringArray; properties: { actual: any } }
+    | { message: ErrorMessage.TypeElementMustBeString; properties: { element: any } }
+    | { message: ErrorMessage.ArrayItemsMustBeStringOrArray; properties: { actual: any } }
+    | { message: ErrorMessage.IDMustHaveAddress, properties: { id: string } }
+    | { message: ErrorMessage.WrongAccessorEntryArrayLength, properties: { operation: string } }
+    | { message: ErrorMessage.SetOperationCasesIsNotArray; properties: { operation: string; cases: any } }
+    | { message: ErrorMessage.CannotFetchSchema; properties: { address: string } }
 
     // Driver
-    | { message: ErrorMessage.UnknownSourceLanguage; props: { lang: string } }
-    | { message: ErrorMessage.UnknownOutputLanguage; props: { lang: string } }
-    | { message: ErrorMessage.NoGraphQLQueryGiven; props: {} }
-    | { message: ErrorMessage.NoGraphQLSchemaInDir; props: { dir: string } }
-    | { message: ErrorMessage.InputFileDoesNotExist; props: { filename: string } }
-    | { message: ErrorMessage.CannotMixJSONWithOtherSamples; props: { dir: string } }
-    | { message: ErrorMessage.CannotMixNonJSONInputs; props: { dir: string } }
-    | { message: ErrorMessage.UnknownDebugOption; props: { option: string } }
+    | { message: ErrorMessage.UnknownSourceLanguage; properties: { lang: string } }
+    | { message: ErrorMessage.UnknownOutputLanguage; properties: { lang: string } }
+    | { message: ErrorMessage.NoGraphQLQueryGiven; properties: {} }
+    | { message: ErrorMessage.NoGraphQLSchemaInDir; properties: { dir: string } }
+    | { message: ErrorMessage.InputFileDoesNotExist; properties: { filename: string } }
+    | { message: ErrorMessage.CannotMixJSONWithOtherSamples; properties: { dir: string } }
+    | { message: ErrorMessage.CannotMixNonJSONInputs; properties: { dir: string } }
+    | { message: ErrorMessage.UnknownDebugOption; properties: { option: string } }
 
     // IR
-    | { message: ErrorMessage.NoForwardDeclarableTypeInCycle; props: {} }
-    | { message: ErrorMessage.TypeAttributesNotPropagated; props: { count: number } }
+    | { message: ErrorMessage.NoForwardDeclarableTypeInCycle; properties: {} }
+    | { message: ErrorMessage.TypeAttributesNotPropagated; properties: { count: number } }
 
     // Rendering
-    | { message: ErrorMessage. UnknownRendererOptionValue; props: { value: string; name: string } },
+    | { message: ErrorMessage. UnknownRendererOptionValue; properties: { value: string; name: string } }
 
     // TypeScript input
-    | { message: ErrorMessage.TypeScriptCompilerError; props: { message: string } }
-    */
+    | { message: ErrorMessage.TypeScriptCompilerError; properties: { message: string } }
+    ;
+*/
 
 /*
 type Errors =
-   { kind: "foo"; props: { quux: number } } |
-   { kind: "bar"; props: { frob: boolean } };
+   { kind: "foo"; properties: { quux: number } } |
+   { kind: "bar"; properties: { frob: boolean } };
 type Kind = Errors extends { kind: infer T } ? T : never;
-type Props = Errors extends { props: infer P } ? P : never;
-type KindFor<P> = Extract<Errors, { props: P }> extends { kind: infer K } ? K : never;
+type properties = Errors extends { properties: infer P } ? P : never;
+type KindFor<P> = Extract<Errors, { properties: P }> extends { kind: infer K } ? K : never;
 
-function error<P extends Props>(kind: KindFor<P>, props: P): void {}
+function error<P extends properties>(kind: KindFor<P>, properties: P): void {}
 */
 
+export class QuickTypeError extends Error {
+    constructor(readonly errorMessage: ErrorMessage, userMessage: string, readonly properties: StringMap) {
+        super(userMessage);
+    }
+}
+
 export function messageError(message: ErrorMessage): never;
-export function messageError(message: ErrorMessage, props: { [name: string]: any }): never;
-export function messageError(message: ErrorMessage, props?: { [name: string]: any }): never {
+export function messageError(message: ErrorMessage, properties: StringMap): never;
+export function messageError(message: ErrorMessage, properties?: StringMap): never {
     let userMessage: string = message;
 
-    if (props !== undefined) {
-        for (const name of Object.getOwnPropertyNames(props)) {
-            let value = props[name];
+    if (properties !== undefined) {
+        for (const name of Object.getOwnPropertyNames(properties)) {
+            let value = properties[name];
             if (typeof value !== "string") {
                 value = JSON.stringify(value);
             }
             userMessage = userMessage.replace("${" + name + "}", value);
         }
+    } else {
+        properties = {};
     }
 
-    throw Error(userMessage);
+    throw new QuickTypeError(message, userMessage, properties);
 }
 
 export function messageAssert(assertion: boolean, message: ErrorMessage): void;
-export function messageAssert(assertion: boolean, message: ErrorMessage, props: { [name: string]: any }): void;
-export function messageAssert(assertion: boolean, message: ErrorMessage, props?: { [name: string]: any }): void {
+export function messageAssert(assertion: boolean, message: ErrorMessage, properties: StringMap): void;
+export function messageAssert(assertion: boolean, message: ErrorMessage, properties?: StringMap): void {
     if (assertion) return;
-    return (messageError as any)(message, props);
+    return (messageError as any)(message, properties);
 }
