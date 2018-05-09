db0a1aac447574f1ef08260a2c9f08cf77673c8d
David Siegel <dasiege@microsoft.com>
2018-05-08 21:50:29 -0700

    Is this an issue with Klaxon?

--- test/fixtures/kotlin/main.kt
+++ test/fixtures/kotlin/main.kt
@@ -5,6 +5,7 @@ import java.io.InputStream
 
 fun main(args: Array<String>) {
 	val json = File("sample.json").readText(Charsets.UTF_8)
-	val top = TopLevel.fromJson(json)!!
-	println(top.toJson())
+	// val top = TopLevel.fromJson(json)!!
+	// println(top.toJson())
+	println(json)
 }
