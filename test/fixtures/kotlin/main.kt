c5c778af2eb390c0959f6f4c0c20bef0ca48fc6f
David Siegel <dasiege@microsoft.com>
2018-05-09 09:38:23 -0700

    Tidy

--- test/fixtures/kotlin/main.kt
+++ test/fixtures/kotlin/main.kt
@@ -4,12 +4,12 @@ import java.io.File
 import java.io.InputStream
 
 fun output(json: String) {
-  val bytes = json.toByteArray()
-  System.out.write(bytes, 0, bytes.size)
+	val bytes = json.toByteArray()
+	System.out.write(bytes, 0, bytes.size)
 }
 
 fun main(args: Array<String>) {
-	val json = File("sample.json").readText(Charsets.UTF_8)
+	val json = File("sample.json").readText()
 	val top = TopLevel.fromJson(json)!!
 	output(top.toJson())
 }
