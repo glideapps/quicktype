6d6330e362629a44dc9d27194bb4d973a83fb230
David Siegel <dasiege@microsoft.com>
2018-05-08 17:59:06 -0700

    Cleaner

--- test/fixtures/kotlin/main.kt
+++ test/fixtures/kotlin/main.kt
@@ -4,9 +4,7 @@ import java.io.File
 import java.io.InputStream
 
 fun main(args: Array<String>) {
-	val stream = File("sample.json").inputStream()
-	val json = stream.bufferedReader(Charsets.UTF_8).use { it.readText() }
-
-	val top = TopLevel.fromJson(json)
-	println(top!!.toJson())
+	val json = File("sample.json").readText(Charsets.UTF_8)
+	val top = TopLevel.fromJson(json)!!
+	println(top.toJson())
 }
