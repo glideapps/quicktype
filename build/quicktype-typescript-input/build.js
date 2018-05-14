dda68738c1c2dbf6c0ea3d6713361f77411e76d7
Mark Probst <mark.probst@gmail.com>
2018-05-14 13:11:42 -0700

    Update packaging stuff

--- build/quicktype-typescript-input/build.js
+++ build/quicktype-typescript-input/build.js
@@ -2,4 +2,4 @@
 
 const buildPackage = require("../build-utils").buildPackage;
 
-buildPackage("../../src/quicktype-typescript-input", "^1.0.0", false);
+buildPackage("../../src/quicktype-typescript-input", "^1.0.1", false);
