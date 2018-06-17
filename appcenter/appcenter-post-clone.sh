98490332eea332d5f2a7d377d294a2394d09c111
David Siegel <dasiege@microsoft.com>
2018-06-17 13:53:25 -0700

    Install node 8.11.3

--- appcenter/appcenter-post-clone.sh
+++ appcenter/appcenter-post-clone.sh
@@ -2,6 +2,10 @@
 
 cd $APPCENTER_SOURCE_DIRECTORY
 
+# Attempt to update node
+curl -O https://nodejs.org/dist/v8.11.3/node-v8.11.3.pkg
+sudo installer -pkg node-v8.11.3.pkg -target /
+
 brew install jq
 
 npm install
