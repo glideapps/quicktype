ed79f4f8ca59c9d0e365cca2887e9c7a20901ac3
Mark Probst <mark.probst@gmail.com>
2018-07-04 13:26:37 -0700

    Fix the CLI on Linux.  Fixes #940
    Problem:
    
    https://stackoverflow.com/questions/4303128/how-to-use-multiple-arguments-for-awk-with-a-shebang-i-e
    
    Solution:
    
    http://sambal.org/2014/02/passing-options-node-shebang-line/

--- script/build.ts
+++ script/build.ts
@@ -7,46 +7,43 @@ import * as _ from "lodash";
 
 const OUTDIR = "dist";
 
-function mapFile(
-  source: string,
-  destination: string,
-  transform: (content: string) => string
-) {
-  const content = fs.readFileSync(source, "utf8");
-  fs.writeFileSync(destination, transform(content));
+function mapFile(source: string, destination: string, transform: (content: string) => string) {
+    const content = fs.readFileSync(source, "utf8");
+    fs.writeFileSync(destination, transform(content));
 }
 
 function installPrereqs() {
-  const result = shell.exec("npm install --ignore-scripts");
-  if (result.code !== 0) {
-    process.exit(result.code);
-  }
+    const result = shell.exec("npm install --ignore-scripts");
+    if (result.code !== 0) {
+        process.exit(result.code);
+    }
 }
 
 function buildTypeScript() {
-  const result = shell.exec(`tsc --project src/cli`);
-  if (result.code !== 0) {
-    process.exit(result.code);
-  }
+    const result = shell.exec(`tsc --project src/cli`);
+    if (result.code !== 0) {
+        process.exit(result.code);
+    }
 }
 
+const scriptPrefix = `#!/bin/sh
+":" //# comment; exec /usr/bin/env node --max-old-space-size=4096 "$0" "$@"
+`;
+
 function makeDistributedCLIExecutable() {
-  const prefix = "#!/usr/bin/env node --max-old-space-size=4096\n";
-  const cli = path.join(OUTDIR, "cli", "index.js");
-  mapFile(cli, cli, content => {
-    if (content.substr(0, prefix.length) === prefix) {
-      return content;
-    }
-    return prefix + content;
-  });
-  shell.chmod("+x", cli);
+    const cli = path.join(OUTDIR, "cli", "index.js");
+    mapFile(cli, cli, content => {
+        if (_.startsWith(content, scriptPrefix)) return content;
+        return scriptPrefix + content;
+    });
+    shell.chmod("+x", cli);
 }
 
 function main() {
-  const skipPrereqs = !!process.env.SKIP_INSTALL_PREREQUISITES;
+    const skipPrereqs = !!process.env.SKIP_INSTALL_PREREQUISITES;
 
-  buildTypeScript();
-  makeDistributedCLIExecutable();
+    buildTypeScript();
+    makeDistributedCLIExecutable();
 }
 
 main();
