5e8ea6eb02463603a97649c0ab04533d88d44418
Mark Probst <mark.probst@gmail.com>
2018-10-16 07:40:04 -0700

    Fix make-pkgs

--- script/make-pkgs.sh
+++ script/make-pkgs.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+MAIN="dist/cli/index.js"
+
 npm install
 
 mkdir -p bin/macos
 mkdir -p bin/linux
 mkdir -p bin/windows
-pkg -t node8-macos-x64 -o bin/macos/quicktype dist/index.js
-pkg -t node8-linux-x64 -o bin/linux/quicktype dist/index.js
-pkg -t node8-win-x64 -o bin/windows/quicktype.exe dist/index.js
+pkg -t node8-macos-x64 -o bin/macos/quicktype "$MAIN"
+pkg -t node8-linux-x64 -o bin/linux/quicktype "$MAIN"
+pkg -t node8-win-x64 -o bin/windows/quicktype.exe "$MAIN"
