c0b24d3f51b236c3ba5372b0e773a235922136fc
Mark Probst <mark.probst@gmail.com>
2018-10-16 23:58:41 -0700

    Remove pkg
    This is the last depender on cryptiles

--- script/make-pkgs.sh
+++ script/make-pkgs.sh
@@ -7,6 +7,6 @@ npm install
 mkdir -p bin/macos
 mkdir -p bin/linux
 mkdir -p bin/windows
-pkg -t node8-macos-x64 -o bin/macos/quicktype "$MAIN"
-pkg -t node8-linux-x64 -o bin/linux/quicktype "$MAIN"
-pkg -t node8-win-x64 -o bin/windows/quicktype.exe "$MAIN"
+npx pkg -t node8-macos-x64 -o bin/macos/quicktype "$MAIN"
+npx pkg -t node8-linux-x64 -o bin/linux/quicktype "$MAIN"
+npx pkg -t node8-win-x64 -o bin/windows/quicktype.exe "$MAIN"
