ac15d4f18f0bdbbf5af2a3a3b239880111127e52
Mark Probst <mark.probst@gmail.com>
2018-11-06 07:46:03 -0800

    Make inference flags more palatable to the web UI

--- src/quicktype-core/Run.ts
+++ src/quicktype-core/Run.ts
@@ -36,52 +36,81 @@ export type RendererOptions = { [name: string]: string };
 export interface InferenceFlag {
     description: string;
     negationDescription: string;
+    explanation: string;
+    order: number;
     stringType?: TransformedStringTypeKind;
 }
 
 export const inferenceFlagsObject = {
-    /** Combine similar classes.  This doesn't apply to classes from a schema, only from inference. */
-    combineClasses: { description: "Merge similar classes", negationDescription: "Don't combine similar classes" },
     /** Whether to infer map types from JSON data */
-    inferMaps: { description: "Detect maps", negationDescription: "Don't infer maps, always use classes" },
+    inferMaps: {
+        description: "Detect maps",
+        negationDescription: "Don't infer maps, always use classes",
+        explanation: "Infer maps when object keys look like map keys.",
+        order: 1
+    },
     /** Whether to infer enum types from JSON data */
-    inferEnums: { description: "Detect enums", negationDescription: "Don't infer enums, always use strings" },
-    /** Whether to assume that JSON strings that look like dates are dates */
-    inferDateTimes: {
-        description: "Detect dates & times",
-        negationDescription: "Don't infer dates or times",
-        stringType: "date-time" as TransformedStringTypeKind
+    inferEnums: {
+        description: "Detect enums",
+        negationDescription: "Don't infer enums, always use strings",
+        explanation: "If string values occur within a relatively small domain,\ninfer them as enum values.",
+        order: 2
     },
     /** Whether to convert UUID strings to UUID objects */
     inferUuids: {
         description: "Detect UUIDs",
         negationDescription: "Don't convert UUIDs to UUID objects",
-        stringType: "uuid" as TransformedStringTypeKind
+        explanation: "Detect UUIDs like '123e4567-e89b-12d3-a456-426655440000' (partial support).",
+        stringType: "uuid" as TransformedStringTypeKind,
+        order: 3
+    },
+    /** Whether to assume that JSON strings that look like dates are dates */
+    inferDateTimes: {
+        description: "Detect dates & times",
+        negationDescription: "Don't infer dates or times",
+        explanation: "Infer dates from strings (partial support).",
+        stringType: "date-time" as TransformedStringTypeKind,
+        order: 4
     },
     /** Whether to convert stringified integers to integers */
     inferIntegerStrings: {
         description: "Detect integers in strings",
         negationDescription: "Don't convert stringified integers to integers",
-        stringType: "integer-string" as TransformedStringTypeKind
+        explanation: 'Automatically convert stringified integers to integers.\nFor example, "1" is converted to 1.',
+        stringType: "integer-string" as TransformedStringTypeKind,
+        order: 5
     },
     /** Whether to convert stringified booleans to boolean values */
     inferBooleanStrings: {
         description: "Detect booleans in strings",
         negationDescription: "Don't convert stringified booleans to booleans",
-        stringType: "bool-string" as TransformedStringTypeKind
+        explanation:
+            'Automatically convert stringified booleans to booleans.\nFor example, "true" is converted to true.',
+        stringType: "bool-string" as TransformedStringTypeKind,
+        order: 6
+    },
+    /** Combine similar classes.  This doesn't apply to classes from a schema, only from inference. */
+    combineClasses: {
+        description: "Merge similar classes",
+        negationDescription: "Don't combine similar classes",
+        explanation:
+            "Combine classes with significantly overlapping properties,\ntreating contingent properties as nullable.",
+        order: 7
     },
     /** Whether to treat $ref as references within JSON */
     ignoreJsonRefs: {
-        description: "Don't handle $ref specially in JSON",
-        negationDescription: "Treat $ref as a reference in JSON"
+        description: "Don't treat $ref as a reference in JSON",
+        negationDescription: "Treat $ref as a reference in JSON",
+        explanation:
+            "Like in JSON Schema, allow objects like\n'{ $ref: \"#/foo/bar\" }' to refer\nto another part of the input.",
+        order: 8
     }
 };
-export const inferenceFlagNames = Object.getOwnPropertyNames(
-    inferenceFlagsObject
-) as (keyof typeof inferenceFlagsObject)[];
-export const inferenceFlags: { [F in keyof typeof inferenceFlagsObject]: InferenceFlag } = inferenceFlagsObject;
+export type InferenceFlagName = keyof typeof inferenceFlagsObject;
+export const inferenceFlagNames = Object.getOwnPropertyNames(inferenceFlagsObject) as InferenceFlagName[];
+export const inferenceFlags: { [F in InferenceFlagName]: InferenceFlag } = inferenceFlagsObject;
 
-export type InferenceFlags = { [F in keyof typeof inferenceFlagsObject]: boolean };
+export type InferenceFlags = { [F in InferenceFlagName]: boolean };
 
 /**
  * The options type for the main quicktype entry points,
@@ -178,18 +207,23 @@ interface GraphInputs {
     typeBuilder: TypeBuilder;
 }
 
+function makeDefaultInferenceFlags(): InferenceFlags {
+    const flags = {} as InferenceFlags;
+    for (const flag of inferenceFlagNames) {
+        flags[flag] = true;
+    }
+    return flags;
+}
+
+export const defaultInferenceFlags = makeDefaultInferenceFlags();
+
 class Run implements RunContext {
     private readonly _options: Options;
 
     constructor(options: Partial<Options>) {
-        const inferenceOptions: { [flag: string]: boolean } = {};
-        for (const flag of inferenceFlagNames) {
-            inferenceOptions[flag] = true;
-        }
-
         // We must not overwrite defaults with undefined values, which
         // we sometimes get.
-        this._options = Object.assign({}, defaultOptions, inferenceOptions as InferenceFlags);
+        this._options = Object.assign({}, defaultOptions, defaultInferenceFlags);
         for (const k of Object.getOwnPropertyNames(options)) {
             const v = (options as any)[k];
             if (v !== undefined) {
