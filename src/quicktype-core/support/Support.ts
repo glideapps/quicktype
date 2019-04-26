d9f004a9cca2ffae81066d9cef629a9ad4e92284
Moritz Bitsch <moritz@h6t.eu>
2019-04-26 15:52:09 +0200

    Add support for yaml
    since we are using a yaml 1.2 compatible parser this is a strict superset of json

--- src/quicktype-core/support/Support.ts
+++ src/quicktype-core/support/Support.ts
@@ -1,6 +1,7 @@
 import { Base64 } from "js-base64";
 import * as pako from "pako";
 import { messageError } from "../Messages";
+import * as YAML from "yaml";
 
 export type StringMap = { [name: string]: any };
 
@@ -110,7 +111,7 @@ export function parseJSON(text: string, description: string, address: string = "
         if (text.charCodeAt(0) === 0xfeff) {
             text = text.slice(1);
         }
-        return JSON.parse(text);
+        return YAML.parse(text);
     } catch (e) {
         let message: string;
 
