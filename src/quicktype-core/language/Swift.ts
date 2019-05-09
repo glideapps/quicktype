4f49725f52ce36f47a7391b7e27bfffa903895c9
Michael Fey <mrrooni@agilebits.com>
2019-05-09 10:53:51 -0400

    Manually repair merge conflict

--- src/quicktype-core/language/Swift.ts
+++ src/quicktype-core/language/Swift.ts
@@ -37,7 +37,7 @@ import {
     camelCase,
     addPrefixIfNecessary
 } from "../support/Strings";
-import { RenderContext } from "../Renderer";
+import { RenderContext, ForEachPosition } from "../Renderer";
 import { StringTypeMapping } from "../TypeBuilder";
 import { panic } from "../support/Support";
 import { DefaultDateTimeRecognizer, DateTimeRecognizer } from "../DateTime";
@@ -97,6 +97,13 @@ class SwiftDateTimeRecognizer extends DefaultDateTimeRecognizer {
     isDateTime(str: string): boolean {
         return str.match(swiftDateTimeRegex) !== null;
     }
+}
+
+export interface SwiftProperty {
+    name: Name;
+    jsonName: string;
+    parameter: ClassProperty;
+    position: ForEachPosition;
 }
 
 export class SwiftTargetLanguage extends TargetLanguage {
@@ -682,20 +689,21 @@ export class SwiftRenderer extends ConvenienceRenderer {
             ) {
                 // Make an initializer that initalizes all fields
                 this.ensureBlankLine();
-                let properties: Sourcelike[] = [];
-                this.forEachClassProperty(c, "none", (name, _, p) => {
-                    if (properties.length > 0) properties.push(", ");
-                    properties.push(name, ": ", this.swiftPropertyType(p));
-                });
+                let initProperties = this.initializableProperties(c);
+                let propertiesLines: Sourcelike[] = [];
+                for (let property of initProperties) {
+                    if (propertiesLines.length > 0) propertiesLines.push(", ");
+                    propertiesLines.push(property.name, ": ", this.swiftPropertyType(property.parameter));
+                }
                 if (this.propertyCount(c) === 0 && this._options.objcSupport) {
                     this.emitBlockWithAccess(["override init()"], () => {
                         "";
                     });
                 } else {
-                    this.emitBlockWithAccess(["init(", ...properties, ")"], () => {
-                        this.forEachClassProperty(c, "none", name => {
-                            this.emitLine("self.", name, " = ", name);
-                        });
+                    this.emitBlockWithAccess(["init(", ...propertiesLines, ")"], () => {
+                        for (let property of initProperties) {
+                            this.emitLine("self.", property.name, " = ", property.name);
+                        }
                     });
                 }
             }
@@ -726,6 +734,15 @@ export class SwiftRenderer extends ConvenienceRenderer {
         }
 
         this.finishFile();
+    }
+
+    protected initializableProperties(c: ClassType): SwiftProperty[] {
+        const properties: SwiftProperty[] = [];
+        this.forEachClassProperty(c, "none", (name, jsonName, parameter, position) => {
+            const property = { name, jsonName, parameter, position };
+            properties.push(property);
+        });
+        return properties;
     }
 
     private emitNewEncoderDecoder(): void {
