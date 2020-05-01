164425e17e40d1326a252a03dbb1404994991e93
Benjamin Jordan <bmj465@gmail.com>
2020-05-01 12:55:45 -0500

    Fixing is required issue.

--- src/quicktype-core/language/JavaScriptPropTypes.ts
+++ src/quicktype-core/language/JavaScriptPropTypes.ts
@@ -1,11 +1,20 @@
 import {TargetLanguage} from "../TargetLanguage";
-import {BooleanOption, getOptionValues, Option, OptionValues} from "../RendererOptions";
+import {getOptionValues, Option, OptionValues} from "../RendererOptions";
 import {RenderContext} from "../Renderer";
 import {ConvenienceRenderer} from "../ConvenienceRenderer";
-import {funPrefixNamer, Namer} from "../Naming";
+import {funPrefixNamer, Name, Namer} from "../Naming";
 import {acronymOption, acronymStyle, AcronymStyleOptions} from "../support/Acronyms";
-import {capitalize, combineWords, firstUpperWordStyle, Sourcelike, splitIntoWords} from "..";
-import {allLowerWordStyle} from "../support/Strings";
+import {
+    capitalize,
+    ClassProperty,
+    combineWords,
+    firstUpperWordStyle,
+    matchType, ObjectType, panic,
+    Sourcelike,
+    splitIntoWords,
+    Type
+} from "..";
+import {allLowerWordStyle, utf16StringEscape} from "../support/Strings";
 import {isES3IdentifierStart} from "./JavaScriptUnicodeMaps";
 import {legalizeName} from "./JavaScript";
 import {convertersOption} from "../support/Converters";
@@ -42,7 +51,7 @@ export class JavaScriptPropTypesRenderer extends ConvenienceRenderer {
     constructor(
         targetLanguage: TargetLanguage,
         renderContext: RenderContext,
-        private readonly _jsOptions: OptionValues<typeof javaScriptOptions>
+        private readonly _jsOptions: OptionValues<typeof javaScriptPropTypesOptions>
     ) {
         super(targetLanguage, renderContext);
     }
@@ -50,7 +59,7 @@ export class JavaScriptPropTypesRenderer extends ConvenienceRenderer {
     protected nameStyle(original: string, upper: boolean): string {
         const acronyms = acronymStyle(this._jsOptions.acronymStyle);
         const words = splitIntoWords(original);
-        return combineWords(
+        const name = combineWords(
             words,
             legalizeName,
             upper ? firstUpperWordStyle : allLowerWordStyle,
@@ -60,13 +69,52 @@ export class JavaScriptPropTypesRenderer extends ConvenienceRenderer {
             "",
             isES3IdentifierStart
         );
+
+        return name;
+    }
+
+    typeMapTypeFor(t: Type, required: boolean = true): Sourcelike {
+        if (["class", "object"].indexOf(t.kind) >= 0) {
+            return this.nameForNamedType(t);
+        }
+
+        const format = (val: Sourcelike) => required ? `${val}.isRequired` : val;
+
+        return format(matchType<Sourcelike>(
+            t,
+            _anyType => `PropTypes.any`,
+            _nullType => panic('There is no null equivalent in PropTypes.'),
+            _boolType => 'PropTypes.bool',
+            _integerType => 'PropTypes.number',
+            _doubleType => 'PropTypes.number',
+            _stringType => 'PropTypes.string',
+            arrayType => `PropTypes.arrayOf(${this.typeMapTypeFor(arrayType.items, false)})`,
+            _classType => panic('Should already be handled.'),
+            _mapType => 'PropTypes.object',
+            enumType => `PropTypes.oneOf(['${Array.from(enumType.cases.values()).join("' ,'")}'])`,
+            unionType => {
+                const children = Array.from(unionType.getChildren()).map((type: Type) => this.typeMapTypeFor(type, false));
+                return `PropTypes.oneOfType([${children.join(',')}])`
+            },
+            _transformedStringType => {
+                return 'PropTypes.string';
+            }
+        ));
+    }
+
+    typeMapTypeForProperty(p: ClassProperty): Sourcelike {
+        const typeMap = this.typeMapTypeFor(p.type);
+        if (!p.isOptional) {
+            return typeMap;
+        }
+        return ["u(undefined, ", typeMap, ")"];
     }
 
     protected emitUsageComments(): void {
         this.emitLine('// For use with proptypes.')
     }
 
-    emitBlock(source: Sourcelike, end: Sourcelike, emit: () => void) {
+    protected emitBlock(source: Sourcelike, end: Sourcelike, emit: () => void) {
         this.emitLine(source, "{");
         this.indent(emit);
         this.emitLine("}", end);
@@ -80,10 +128,18 @@ export class JavaScriptPropTypesRenderer extends ConvenienceRenderer {
     protected emitTypes(): void {
         this.ensureBlankLine();
 
-        this.forEachTopLevel("none", (_, name) => {
-            this.emitBlock(`export const ${name} = {`, '};', () => {
-
+        this.forEachObject("none", (t: ObjectType, name: Name) => {
+            this.emitLine('export const ', name, ' = PropTypes.shape({')
+            this.indent(() => {
+                this.forEachClassProperty(t, "none", (_, jsonName, property) => {
+                    this.emitLine(
+                        utf16StringEscape(jsonName),
+                        ': ',
+                        this.typeMapTypeForProperty(property),
+                        ',');
+                });
             });
+            this.emitLine('});');
         });
     }
 
@@ -114,5 +170,4 @@ export class JavaScriptPropTypesRenderer extends ConvenienceRenderer {
     protected namerForObjectProperty(): Namer | null {
         return identityNamingFunction;
     }
-
 }
