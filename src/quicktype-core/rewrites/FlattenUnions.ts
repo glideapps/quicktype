5a4a42e05188670b0c6eb994ca06aff03e48042a
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-05-05 16:55:44 +0200

    run ci to see what happens when unions are never flattened

--- src/quicktype-core/rewrites/FlattenUnions.ts
+++ src/quicktype-core/rewrites/FlattenUnions.ts
@@ -37,7 +37,7 @@ export function flattenUnions(
     let foundIntersection = false;
     const groups = makeGroupsToFlatten(nonCanonicalUnions, members => {
         messageAssert(members.size > 0, "IRNoEmptyUnions", {});
-        if (!iterableSome(members, m => m instanceof IntersectionType)) return true;
+        if (!iterableSome(members, m => m instanceof IntersectionType)) return false;
 
         // FIXME: This is stupid.  `flattenUnions` returns true when no more union
         // flattening is necessary, but `resolveIntersections` can introduce new
