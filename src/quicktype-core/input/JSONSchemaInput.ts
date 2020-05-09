6a376ad3c9becdbbcbdcf3e805c641c59079fe01
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-05-09 10:51:45 +0200

    rename to quicktypePropertyOrder

--- src/quicktype-core/input/JSONSchemaInput.ts
+++ src/quicktype-core/input/JSONSchemaInput.ts
@@ -847,7 +847,7 @@ async function addTypesInSchema(
                 inferredAttributes,
                 combineProducedAttributes(({ forObject }) => forObject)
             );
-            const order = schema.propertyOrder ? schema.propertyOrder : [];
+            const order = schema.quicktypePropertyOrder ? schema.quicktypePropertyOrder : [];
             const orderKey = (propertyName: string) => {
                 // use the index of the order array
                 const index = order.indexOf(propertyName);
