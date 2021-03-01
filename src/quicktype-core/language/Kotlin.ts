b6c7ad7e5bacbe21872358acfc2c7b845d3a5cc8
0us <larsoyvind28@gmail.com>
2021-03-01 10:39:06 +0100

    fix unused enum serializer bug

--- src/quicktype-core/language/Kotlin.ts
+++ src/quicktype-core/language/Kotlin.ts
@@ -1080,7 +1080,7 @@ export class KotlinXRenderer extends KotlinRenderer {
     protected emitEnumDefinition(e: EnumType, enumName: Name): void {
         this.emitDescription(this.descriptionForType(e));
 
-        this.emitLine(["@Serializable"]);
+        this.emitLine(["@Serializable(with = ", enumName, ".Companion::class)"]);
         this.emitBlock(["enum class ", enumName, "(val value: String)"], () => {
             let count = e.cases.size;
             this.forEachEnumCase(e, "none", (name, json) => {
