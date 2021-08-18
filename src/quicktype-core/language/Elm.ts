ef0b6440b2646ca9494d04b4ca8f36fad114ba50
Steven Vandevelde <icid.asset@gmail.com>
2021-08-18 16:52:31 +0200

    Try to address deepsource.io feedback

--- src/quicktype-core/language/Elm.ts
+++ src/quicktype-core/language/Elm.ts
@@ -4,7 +4,7 @@ import { anyTypeIssueAnnotation, nullTypeIssueAnnotation } from "../Annotation";
 import { ConvenienceRenderer, ForbiddenWordsInfo } from "../ConvenienceRenderer";
 import { Namer, Name, DependencyName, funPrefixNamer } from "../Naming";
 import { RenderContext } from "../Renderer";
-import { EnumOption, StringOption, BooleanOption, Option, getOptionValues, OptionValues } from "../RendererOptions";
+import { EnumOption, StringOption, BooleanOption, Option, getOptionValues } from "../RendererOptions";
 import { Sourcelike, annotated, MultiWord, singleWord, multiWord, parenIfNeeded } from "../Source";
 import { TargetLanguage } from "../TargetLanguage";
 import { Type, ClassType, UnionType, EnumType, ClassProperty } from "../Type";
@@ -143,13 +143,6 @@ function requiredOrOptional(p: ClassProperty): RequiredOrOptional {
 export class ElmRenderer extends ConvenienceRenderer {
     private readonly _topLevelDependents = new Map<Name, TopLevelDependent>();
     private readonly _namedTypeDependents = new Map<Name, NamedTypeDependent>();
-
-    constructor(
-        targetLanguage: TargetLanguage,
-        renderContext: RenderContext
-    ) {
-        super(targetLanguage, renderContext);
-    }
 
     // NAMES
     // -----
