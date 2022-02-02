4393f71dea3b8227c98db0cfba7d1868205cbac5
Mark Probst <mark.probst@gmail.com>
2022-02-02 05:15:50 -0500

    Disable C++ tests that take too much memory to compile

--- test/languages.ts
+++ test/languages.ts
@@ -90,11 +90,7 @@ export const JavaLanguage: Language = {
   diffViaSchema: false,
   skipDiffViaSchema: [],
   allowMissingNull: false,
-  features: [
-    "enum",
-    "union",
-    "uuid"
-  ],
+  features: ["enum", "union", "uuid"],
   output: "src/main/java/io/quicktype/TopLevel.java",
   topLevel: "TopLevel",
   skipJSON: ["identifiers.json", "simple-identifiers.json", "nst-test-suite.json"],
@@ -117,14 +113,14 @@ export const JavaLanguageWithLegacyDateTime: Language = {
     "337ed.json", // Expects less strict serialization (optional milliseconds).
   ],
   skipMiscJSON: true, // Handles edge cases differently and does not allow optional milliseconds.
-  rendererOptions: {"datetime-provider": "legacy"},
-  quickTestRendererOptions: [{ "array-type": "list"}],
+  rendererOptions: { "datetime-provider": "legacy" },
+  quickTestRendererOptions: [{ "array-type": "list" }],
 };
 
 export const JavaLanguageWithLombok: Language = {
   ...JavaLanguage,
   base: "test/fixtures/java-lombok",
-  quickTestRendererOptions: [{ "array-type": "list", "lombok": "true"}],
+  quickTestRendererOptions: [{ "array-type": "list", lombok: "true" }],
 };
 
 export const PythonLanguage: Language = {
@@ -397,6 +393,12 @@ export const CPlusPlusLanguage: Language = {
     "nst-test-suite.json",
     // compiler error I don't want to figure out right now
     "nbl-stats.json",
+    // uses too much memory compiling
+    "combinations.json",
+    "combinations1.json",
+    "combinations2.json",
+    "combinations3.json",
+    "combinations4.json",
   ],
   skipMiscJSON: false,
   skipSchema: [],
@@ -1037,7 +1039,7 @@ export const HaskellLanguage: Language = {
     "recursive.json",
     "bug427.json",
     "nst-test-suite.json",
-    "keywords.json"
+    "keywords.json",
   ],
   skipMiscJSON: false,
   skipSchema: [
@@ -1056,5 +1058,5 @@ export const HaskellLanguage: Language = {
   ],
   rendererOptions: {},
   quickTestRendererOptions: [{ "array-type": "list" }],
-  sourceFiles: ["src/language/Haskell.ts"]
+  sourceFiles: ["src/language/Haskell.ts"],
 };
