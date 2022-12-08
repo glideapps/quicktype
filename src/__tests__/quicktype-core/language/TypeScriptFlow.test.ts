fa30fbf55b79ac48ee3bd3b37b4a71da201e3057
René Werner <rene@gofiber.io>
2022-12-08 17:19:21 +0100

    AdditionalProperties ignored for Typescript definition generation #1615  https://github.com/quicktype/quicktype/issues/1615

--- src/__tests__/quicktype-core/language/TypeScriptFlow.test.ts
+++ src/__tests__/quicktype-core/language/TypeScriptFlow.test.ts
@@ -2,39 +2,58 @@ import { readFileSync } from "fs";
 import { InputData, JSONSchemaInput, quicktype } from "../../../quicktype-core";
 import { resolve } from "path";
 
-describe("json to ts", () => {
-    it("should convert with additionalProperties", async () => {
-        const schema = readFileSync(resolve(`${__dirname}/../../../../test/inputs/schema/class-with-additional.schema`), "utf8");
-        const schemaInput = new JSONSchemaInput(undefined, []);
-        await schemaInput.addSource({ name: "Test", schema});
+const setupAndRunConvert = async (file: string) => {
+    const schema = readFileSync(resolve(file), "utf8");
+    const schemaInput = new JSONSchemaInput(undefined, []);
+    await schemaInput.addSource({ name: "Test", schema});
 
-        const inputData = new InputData();
-        inputData.addInput(schemaInput);
+    const inputData = new InputData();
+    inputData.addInput(schemaInput);
 
-        const result = await quicktype({
-            inputData,
-            lang: "typescript",
-            checkProvenance: false,
-            inferDateTimes: true,
-            inferBooleanStrings: true,
-            inferIntegerStrings: true,
-            inferMaps: true,
-            inferUuids: true,
-            alphabetizeProperties: true,
-            inferEnums: true,
-            rendererOptions: {
-                "just-types": "true",
-                "runtime-check": "true",
-            },
-        });
+    return (await quicktype({
+        inputData,
+        lang: "typescript",
+        checkProvenance: false,
+        inferDateTimes: true,
+        inferBooleanStrings: true,
+        inferIntegerStrings: true,
+        inferMaps: true,
+        inferUuids: true,
+        alphabetizeProperties: true,
+        inferEnums: true,
+        rendererOptions: {
+            "just-types": "true",
+            "runtime-check": "true",
+        },
+    })).lines.join("\n");
+};
+describe("jsonSchema to ts", () => {
 
-        expect(result.lines.join("\n")).toBe(`export interface Test {
+    it("should convert with additionalProperties and specific type", async () => {
+        const output = await setupAndRunConvert(`${__dirname}/../../../../test/inputs/schema/class-with-additional.schema`);
+
+        expect(output).toBe(`export interface Test {
     map?: Map;
 }
 
 export interface Map {
     foo?: number;
     [k: string]: bool;
+}
+`);
+    });
+
+    it("should convert with additionalProperties and any type", async () => {
+        const output = await setupAndRunConvert(`${__dirname}/../../../../test/inputs/schema/intersection.schema`);
+
+        expect(output).toBe(`export interface Test {
+    intersection?: Intersection;
+}
+
+export interface Intersection {
+    bar?: string;
+    foo:  number;
+    [k: string]: any;
 }
 `);
     });
