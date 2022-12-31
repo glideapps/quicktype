d5e0ea24b70a54251e9c902ddd0e3075bba967e6
David Siegel <djsiegel@gmail.com>
2022-12-31 16:44:36 +0000

    Try to run only affected fixtures

--- test/test.ts
+++ test/test.ts
@@ -16,29 +16,28 @@ const CPUs = parseInt(process.env.CPUs || "0", 10) || os.cpus().length;
 export type WorkItem = { sample: Sample; fixtureName: string };
 
 async function main(sources: string[]) {
-  let fixtures = allFixtures;
+  let fixtures = affectedFixtures();
   const fixturesFromCmdline = process.env.FIXTURE;
   if (fixturesFromCmdline) {
     const fixtureNames = fixturesFromCmdline.split(",");
-    fixtures = _.filter(fixtures, fixture => _.some(fixtureNames, name => fixture.runForName(name)));
-  } else {
-    fixtures = affectedFixtures();
-    if (allFixtures.length !== fixtures.length) {
-      console.error(`* Running a subset of fixtures: ${fixtures.map(f => f.name).join(", ")}`);
-    }
+    fixtures = _.filter(fixtures, (fixture) => _.some(fixtureNames, (name) => fixture.runForName(name)));
+  }
+
+  if (allFixtures.length !== fixtures.length) {
+    console.error(`* Running a subset of fixtures: ${fixtures.map((f) => f.name).join(", ")}`);
   }
   // Get an array of all { sample, fixtureName } objects we'll run.
   // We can't just put the fixture in there because these WorkItems
   // will be sent in a message, removing all code.
-  const samples = _.map(fixtures, fixture => ({
+  const samples = _.map(fixtures, (fixture) => ({
     fixtureName: fixture.name,
-    samples: fixture.getSamples(sources)
+    samples: fixture.getSamples(sources),
   }));
-  const priority = _.flatMap(samples, x =>
-    _.map(x.samples.priority, s => ({ fixtureName: x.fixtureName, sample: s }))
+  const priority = _.flatMap(samples, (x) =>
+    _.map(x.samples.priority, (s) => ({ fixtureName: x.fixtureName, sample: s }))
   );
-  const others = _.flatMap(samples, x =>
-    _.map(x.samples.others, s => ({ fixtureName: x.fixtureName, sample: s }))
+  const others = _.flatMap(samples, (x) =>
+    _.map(x.samples.others, (s) => ({ fixtureName: x.fixtureName, sample: s }))
   );
 
   const tests = divideParallelJobs(_.concat(priority, others));
@@ -68,7 +67,7 @@ async function main(sources: string[]) {
         console.trace(e);
         exit(1);
       }
-    }
+    },
   });
 }
 
@@ -81,7 +80,7 @@ function testCLI() {
 }
 
 // skip 2 `node` args
-main(process.argv.slice(2)).catch(reason => {
+main(process.argv.slice(2)).catch((reason) => {
   console.error(reason);
   process.exit(1);
 });
