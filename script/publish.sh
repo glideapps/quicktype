20296629979c6c4d783f1b02075da80eb00ba166
David Siegel <djsiegel@gmail.com>
2023-01-14 21:06:54 -0800

    19.0.4

--- script/publish.sh
+++ script/publish.sh
@@ -3,5 +3,18 @@
 VERSION=$(npm version patch --force)
 npm version $VERSION --workspaces --force
 
+# This is not great, but we need to get the dependencies to workspaces
+jq --arg version $VERSION \
+    '.dependencies."quicktype-core" = $version | .dependencies."quicktype-graphql-input" = $version | .dependencies."quicktype-typescript-input" = $version' \
+    package.json > package.1.json
+mv package.1.json package.json
+
 npm publish
+
+
+jq --arg version $VERSION \
+    '.dependencies."quicktype-core" = $version' \
+    packages/quicktype-typescript-input/package.json > package.1.json
+mv package.1.json packages/quicktype-typescript-input/package.json
+
 npm publish --workspaces --if-present
