57ca58509242342bb25659089ba5988b39be81dc
David Siegel <djsiegel@gmail.com>
2023-01-14 19:58:17 -0800

    sync versions

--- script/publish.sh
+++ script/publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-./script/patch-npm-version.ts
+VERSION=$(npm version patch --force)
+npm version $VERSION --workspaces --force
 
 npm publish --dry-run
 npm publish --workspaces --if-present --dry-run
