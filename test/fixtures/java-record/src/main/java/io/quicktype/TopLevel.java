c198bc4cfeea9acec1f65edd5457a867e62ef417
David Siegel <dvdsgl@users.noreply.github.com>
2023-02-15 06:30:51 -0800

    Revert "Add support for Java Records (JEP 395) (#2165)"
    This reverts commit 0fc948fdbe40f7d588920e3f59dfa9daa1054f4b.

--- test/fixtures/java-record/src/main/java/io/quicktype/TopLevel.java
+++
@@ -1,4 +0,0 @@
-package io.quicktype;
-
-public class TopLevel {
-}
--- test/languages.ts
+++ test/languages.ts
@@ -108,7 +108,7 @@ export const CSharpLanguageSystemTextJson: Language = {
 export const JavaLanguage: Language = {
     name: "java",
     base: "test/fixtures/java",
-    setupCommand: "mvn --version",
+    setupCommand: "mvn package",
     compileCommand: "mvn package",
     runCommand(sample: string) {
         return `java -cp target/QuickTypeTest-1.0-SNAPSHOT.jar io.quicktype.App "${sample}"`;
@@ -147,13 +147,7 @@ export const JavaLanguageWithLegacyDateTime: Language = {
 export const JavaLanguageWithLombok: Language = {
     ...JavaLanguage,
     base: "test/fixtures/java-lombok",
-    rendererOptions: [{ "array-type": "list", "class-style": "lombok" }]
-};
-
-export const JavaLanguageWithRecord: Language = {
-    ...JavaLanguage,
-    base: "test/fixtures/java-record",
-    rendererOptions: [{ "array-type": "list", "class-style": "java-record" }]
+    quickTestRendererOptions: [{ "array-type": "list", lombok: "true" }]
 };
 
 export const PythonLanguage: Language = {
