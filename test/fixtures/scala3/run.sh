d7f78f5920a9fa5ad9eebc08401f71ce872a0aaa
Simon Parten <Quafadas@users.noreply.github.com>
2023-02-22 14:58:10 +0100

    Back to simplicity with new scala-cli release

--- test/fixtures/scala3/run.sh
+++ test/fixtures/scala3/run.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-wget https://github.com/VirtusLab/scala-cli/releases/download/nightly/scala-cli-x86_64-pc-linux-static.gz
-gunzip scala-cli-x86_64-pc-linux-static.gz
-chmod +x scala-cli-x86_64-pc-linux-static
-./scala-cli-x86_64-pc-linux-static  -v -v -v circe.scala TopLevel.scala 
+scala-cli circe.scala TopLevel.scala
