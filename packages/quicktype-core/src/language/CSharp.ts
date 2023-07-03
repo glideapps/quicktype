590738a1d5eab1ce62e8e5fd1117b24c9534251a
Icarus <2654808955@qq.com>
2023-07-03 20:56:06 +0800

    !B(Core.Language.CSharp) `keywords`

--- packages/quicktype-core/src/language/CSharp.ts
+++ packages/quicktype-core/src/language/CSharp.ts
@@ -306,13 +306,95 @@ function csNameStyle(original: string): string {
 }
 
 function csNameStyleKeep(original: string): string {
+
+    const keywords = [
+        "abstract",
+        "as",
+        "base",
+        "bool",
+        "break",
+        "byte",
+        "case",
+        "catch",
+        "char",
+        "checked",
+        "class",
+        "const",
+        "continue",
+        "decimal",
+        "default",
+        "delegate",
+        "do",
+        "double",
+        "else",
+        "enum",
+        "event",
+        "explicit",
+        "extern",
+        "false",
+        "finally",
+        "fixed",
+        "float",
+        "for",
+        "foreach",
+        "goto",
+        "if",
+        "implicit",
+        "in",
+        "int",
+        "interface",
+        "internal",
+        "is",
+        "lock",
+        "long",
+        "namespace",
+        "new",
+        "null",
+        "object",
+        "operator",
+        "out",
+        "override",
+        "params",
+        "private",
+        "protected",
+        "public",
+        "readonly",
+        "ref",
+        "return",
+        "sbyte",
+        "sealed",
+        "short",
+        "sizeof",
+        "stackalloc",
+        "static",
+        "string",
+        "struct",
+        "switch",
+        "this",
+        "throw",
+        "true",
+        "try",
+        "typeof",
+        "uint",
+        "ulong",
+        "unchecked",
+        "unsafe",
+        "ushort",
+        "using",
+        "virtual",
+        "void",
+        "volatile",
+        "while",
+    ]
+
     const words: WordInName[] = [
         {
             word: original,
             isAcronym: false
         }
     ];
-    return combineWords(
+
+    const result = combineWords(
         words,
         legalizeName,
         x => x,
@@ -321,7 +403,9 @@ function csNameStyleKeep(original: string): string {
         x => x,
         "",
         isStartCharacter
-    );
+    )
+
+    return keywords.includes(result) ? "@" + result : result;
 }
 
 function isValueType(t: Type): boolean {
