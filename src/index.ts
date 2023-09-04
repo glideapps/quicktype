8a602d3fe18e524829a2f4af760274c83baafcc7
Viktor Stepanov <108535441+U3erKa@users.noreply.github.com>
2023-09-04 21:25:54 +0300

    fix: increase --help output width from 80 to 120 (#2391)

--- src/index.ts
+++ src/index.ts
@@ -347,7 +347,7 @@ function makeOptionDefinitions(targetLanguages: TargetLanguage[]): OptionDefinit
                       name: "lang",
                       alias: "l",
                       type: String,
-                      typeLabel: makeLangTypeLabel(targetLanguages),
+                      typeLabel: "LANG",
                       description: "The target language."
                   }
               ];
@@ -357,7 +357,7 @@ function makeOptionDefinitions(targetLanguages: TargetLanguage[]): OptionDefinit
             alias: "s",
             type: String,
             defaultValue: undefined,
-            typeLabel: "json|schema|graphql|postman|typescript",
+            typeLabel: "SRC_LANG",
             description: "The source language (default is json)."
         },
         {
@@ -495,10 +495,11 @@ const tableOptionsForOptions: TableOptions = {
     columns: [
         {
             name: "option",
-            width: 50
+            width: 60
         },
         {
-            name: "description"
+            name: "description",
+            width: 60
         }
     ]
 };
@@ -510,9 +511,13 @@ function makeSectionsBeforeRenderers(targetLanguages: TargetLanguage[]): UsageSe
         {
             header: "Synopsis",
             content: [
-                `$ quicktype [${chalk.bold("--lang")} LANG] [${chalk.bold("--out")} FILE] FILE|URL ...`,
+                `$ quicktype [${chalk.bold("--lang")} LANG] [${chalk.bold("--src-lang")} SRC_LANG] [${chalk.bold(
+                    "--out"
+                )} FILE] FILE|URL ...`,
                 "",
-                `  LANG ... ${makeLangTypeLabel(targetLanguages)}`
+                `  LANG ... ${makeLangTypeLabel(targetLanguages)}`,
+                "",
+                "SRC_LANG ... json|schema|graphql|postman|typescript"
             ]
         },
         {
