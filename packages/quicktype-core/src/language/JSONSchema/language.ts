a188f7976342e3349226841253caa217165e3b5e
inferrinizzard <inferrinizzard@gmail.com>
2024-05-12 15:23:36 -0700

    :recycle: fix imports

--- packages/quicktype-core/src/language/JSONSchema/language.ts
+++ packages/quicktype-core/src/language/JSONSchema/language.ts
@@ -2,7 +2,7 @@ import { type RenderContext } from "../../Renderer";
 import { type Option } from "../../RendererOptions";
 import { TargetLanguage } from "../../TargetLanguage";
 import { type StringTypeMapping, getNoStringTypeMapping } from "../../TypeBuilder";
-import { type FixMeOptionsAnyType } from "../../types";
+import { type FixMeOptionsAnyType, type FixMeOptionsType } from "../../types";
 
 import { JSONSchemaRenderer } from "./JSONSchemaRenderer";
 
