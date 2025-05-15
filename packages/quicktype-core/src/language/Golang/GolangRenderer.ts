8c864caa8d531df24cc70ceaac4e6d845ea5adae
inferrinizzard <inferrinizzard@gmail.com>
2025-05-15 08:59:23 -0700

    fix: go multiline

--- packages/quicktype-core/src/language/Golang/GolangRenderer.ts
+++ packages/quicktype-core/src/language/Golang/GolangRenderer.ts
@@ -489,117 +489,116 @@ export class GoRenderer extends ConvenienceRenderer {
             this.emitPackageDefinitons(true, imports);
             this.ensureBlankLine();
             this.emitMultiline(`func unmarshalUnion(data []byte, pi **int64, pf **float64, pb **bool, ps **string, haveArray bool, pa interface{}, haveObject bool, pc interface{}, haveMap bool, pm interface{}, haveEnum bool, pe interface{}, nullable bool) (bool, error) {
- if pi != nil {
-   *pi = nil
- }
- if pf != nil {
-   *pf = nil
- }
- if pb != nil {
-   *pb = nil
- }
- if ps != nil {
-   *ps = nil
- }
+	if pi != nil {
+			*pi = nil
+	}
+	if pf != nil {
+			*pf = nil
+	}
+	if pb != nil {
+			*pb = nil
+	}
+	if ps != nil {
+			*ps = nil
+	}
 
- dec := json.NewDecoder(bytes.NewReader(data))
- dec.UseNumber()
- tok, err := dec.Token()
- if err != nil {
-   return false, err
- }
+	dec := json.NewDecoder(bytes.NewReader(data))
+	dec.UseNumber()
+	tok, err := dec.Token()
+	if err != nil {
+			return false, err
+	}
 
- switch v := tok.(type) {
- case json.Number:
-   if pi != nil {
-     i, err := v.Int64()
-     if err == nil {
-       *pi = &i
-       return false, nil
-     }
-   }
-   if pf != nil {
-     f, err := v.Float64()
-     if err == nil {
-       *pf = &f
-       return false, nil
-     }
-     return false, errors.New("Unparsable number")
-   }
-   return false, errors.New("Union does not contain number")
- case float64:
-   return false, errors.New("Decoder should not return float64")
- case bool:
-   if pb != nil {
-     *pb = &v
-     return false, nil
-   }
-   return false, errors.New("Union does not contain bool")
- case string:
-   if haveEnum {
-     return false, json.Unmarshal(data, pe)
-   }
-   if ps != nil {
-     *ps = &v
-     return false, nil
-   }
-   return false, errors.New("Union does not contain string")
- case nil:
-   if nullable {
-     return false, nil
-   }
-   return false, errors.New("Union does not contain null")
- case json.Delim:
-   if v == '{' {
-     if haveObject {
-       return true, json.Unmarshal(data, pc)
-     }
-     if haveMap {
-       return false, json.Unmarshal(data, pm)
-     }
-     return false, errors.New("Union does not contain object")
-   }
-   if v == '[' {
-     if haveArray {
-       return false, json.Unmarshal(data, pa)
-     }
-     return false, errors.New("Union does not contain array")
-   }
-   return false, errors.New("Cannot handle delimiter")
- }
- return false, errors.New("Cannot unmarshal union")
-
+	switch v := tok.(type) {
+	case json.Number:
+			if pi != nil {
+					i, err := v.Int64()
+					if err == nil {
+							*pi = &i
+							return false, nil
+					}
+			}
+			if pf != nil {
+					f, err := v.Float64()
+					if err == nil {
+							*pf = &f
+							return false, nil
+					}
+					return false, errors.New("Unparsable number")
+			}
+			return false, errors.New("Union does not contain number")
+	case float64:
+			return false, errors.New("Decoder should not return float64")
+	case bool:
+			if pb != nil {
+					*pb = &v
+					return false, nil
+			}
+			return false, errors.New("Union does not contain bool")
+	case string:
+			if haveEnum {
+					return false, json.Unmarshal(data, pe)
+			}
+			if ps != nil {
+					*ps = &v
+					return false, nil
+			}
+			return false, errors.New("Union does not contain string")
+	case nil:
+			if nullable {
+					return false, nil
+			}
+			return false, errors.New("Union does not contain null")
+	case json.Delim:
+			if v == '{' {
+					if haveObject {
+							return true, json.Unmarshal(data, pc)
+					}
+					if haveMap {
+							return false, json.Unmarshal(data, pm)
+					}
+					return false, errors.New("Union does not contain object")
+			}
+			if v == '[' {
+					if haveArray {
+							return false, json.Unmarshal(data, pa)
+					}
+					return false, errors.New("Union does not contain array")
+			}
+			return false, errors.New("Cannot handle delimiter")
+	}
+	return false, errors.New("Cannot unmarshal union")
 }
 
 func marshalUnion(pi *int64, pf *float64, pb *bool, ps *string, haveArray bool, pa interface{}, haveObject bool, pc interface{}, haveMap bool, pm interface{}, haveEnum bool, pe interface{}, nullable bool) ([]byte, error) {
- if pi != nil {
-   return json.Marshal(*pi)
- }
- if pf != nil {
-   return json.Marshal(*pf)
- }
- if pb != nil {
-   return json.Marshal(*pb)
- }
- if ps != nil {
-   return json.Marshal(*ps)
- }
- if haveArray {
-   return json.Marshal(pa)
- }
- if haveObject {
-   return json.Marshal(pc)
- }
- if haveMap {
-   return json.Marshal(pm)
- }
- if haveEnum {
-   return json.Marshal(pe)
- }
- if nullable {
-   return json.Marshal(nil)
- }
- return nil, errors.New("Union must not be null")
+	if pi != nil {
+			return json.Marshal(*pi)
+	}
+	if pf != nil {
+			return json.Marshal(*pf)
+	}
+	if pb != nil {
+			return json.Marshal(*pb)
+	}
+	if ps != nil {
+			return json.Marshal(*ps)
+	}
+	if haveArray {
+			return json.Marshal(pa)
+	}
+	if haveObject {
+			return json.Marshal(pc)
+	}
+	if haveMap {
+			return json.Marshal(pm)
+	}
+	if haveEnum {
+			return json.Marshal(pe)
+	}
+	if nullable {
+			return json.Marshal(nil)
+	}
+	return nil, errors.New("Union must not be null")
 }`);
             this.endFile();
         }
