bcb6f9cb838cf70bb3784be8c0c78ef9395e58cf
inferrinizzard <inferrinizzard@gmail.com>
2025-05-17 09:23:19 -0700

    add grep replace for ci

--- packages/quicktype-core/env.sh
+++ packages/quicktype-core/env.sh
@@ -2,10 +2,7 @@
 
 if [[ $CI ]]
 then
-	echo 'FROG 1'
-	# grep -rl '$fetch' src | xargs sed -i '' -e 's/$fetch/$fetch.ci/g'
-else
-	echo 'FROG 2'
+	grep -rl '$fetch' src | xargs sed -i '' -e 's/$fetch/$fetch.ci/g'
 fi
 
 exit 0
