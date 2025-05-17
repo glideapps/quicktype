0633bf28ad2518f4c0f38b42658d138532acd5be
inferrinizzard <inferrinizzard@gmail.com>
2025-05-17 10:14:14 -0700

    revert test

--- packages/quicktype-core/env.sh
+++ packages/quicktype-core/env.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo $CI
-echo $NODE_AUTH_TOKEN
-
-
 if [[ $NODE_AUTH_TOKEN ]]
 then
 	echo 'HAS NODE_AUTH_TOKEN, exit'
@@ -12,9 +8,7 @@ fi
 
 if [[ $CI ]]
 then
-	echo 'grep'
 	grep -rl '$fetch' src | xargs sed -i '' -e 's/$fetch/$fetch.ci/g'
 fi
 
-echo 'default exit'
 exit 0
