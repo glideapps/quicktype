6acefce1cb3a09f83a9ccfbc7e2f4db42fb3a90e
Mark Probst <mark.probst@gmail.com>
2026-08-28 13:00:38 -0700

    test(crystal): enable enum schema positives

--- test/languages.ts
+++ test/languages.ts
@@ -398,7 +398,7 @@ export const CrystalLanguage: Language = {
     diffViaSchema: false,
     skipDiffViaSchema: [],
     allowMissingNull: true,
-    features: ["enum", "union", "no-defaults"],
+    features: ["union", "no-defaults"],
     output: "TopLevel.cr",
     topLevel: "TopLevel",
     skipJSON: [
@@ -409,10 +409,6 @@ export const CrystalLanguage: Language = {
     ],
     skipSchema: [
         "integer-type.schema", // Crystal integers are Int32.
-        // Crystal does not handle enum mapping
-        ...skipsEnumValueValidation.filter(
-            (schema) => schema !== "optional-enum.schema",
-        ),
         "keyword-unions.schema",
     ],
     skipMiscJSON: false,
