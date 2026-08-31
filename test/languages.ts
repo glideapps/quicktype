b51ddd1193e1dcd177a139970dcb1ec8b6666dda
Mark Probst <mark.probst@gmail.com>
2026-08-30 20:49:40 -0700

    fix(objective-c): validate date strings

--- test/languages.ts
+++ test/languages.ts
@@ -937,6 +937,7 @@ export const ObjectiveCLanguage: Language = {
         "minmaxlength",
         "no-defaults",
         "strict-optional",
+        "date-time",
     ],
     output: "QTTopLevel.m",
     topLevel: "QTTopLevel",
