fdf76d177cf2d262d5967ee3c4e14c5effe3268d
Mark Probst <mark.probst@gmail.com>
2026-09-05 14:21:01 -0700

    fix(pike): reject zero as a string enum

--- packages/quicktype-core/src/language/Pike/PikeRenderer.ts
+++ packages/quicktype-core/src/language/Pike/PikeRenderer.ts
@@ -173,7 +173,7 @@ export class PikeRenderer extends ConvenienceRenderer {
             this.emitTable(table);
         });
         this.ensureBlankLine();
-        const check = `if(json&&${checks.join("&&")})error("enum");`;
+        const check = `if(!zero_type(json)&&${checks.join("&&")})error("enum");`;
         this.emitBlock(
             [enumName, " ", enumName, "_from_JSON(mixed json)"],
             () => this.emitLine(check, "return json;"),
