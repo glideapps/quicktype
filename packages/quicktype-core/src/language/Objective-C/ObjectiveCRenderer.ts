b7096c6ce2a6fe7a25396501065306d2c37c8046
Mark Probst <mark.probst@gmail.com>
2026-09-05 09:51:46 -0700

    fix(objective-c): avoid restricted callback parameter name

--- packages/quicktype-core/src/language/Objective-C/ObjectiveCRenderer.ts
+++ packages/quicktype-core/src/language/Objective-C/ObjectiveCRenderer.ts
@@ -59,9 +59,9 @@ type MemoryAttribute = "assign" | "strong" | "copy";
 const objectiveCStringEscape = (s: string): string =>
     stringEscape(s).replace(
         /\\\\|\\u00([0-9a-f]{2})/g,
-        (escape, code: string | undefined) =>
+        (escaped, code: string | undefined) =>
             code === undefined
-                ? escape
+                ? escaped
                 : Array.from(
                       new TextEncoder().encode(
                           String.fromCharCode(Number.parseInt(code, 16)),
