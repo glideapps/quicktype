0e73b04a5cbbb5e61eed22cd889198043eeb6861
Mark Probst <mark.probst@gmail.com>
2026-09-05 14:52:34 -0700

    Keep unsupported Elixir optional schema skipped

--- test/languages.ts
+++ test/languages.ts
@@ -105,7 +105,7 @@ export const JSONSchemaLanguage: Language = {
     topLevel: "TopLevel",
     skipJSON: [],
     skipMiscJSON: false,
-    skipSchema: [],
+    skipSchema: ["optional-property.schema"],
     rendererOptions: {},
     quickTestRendererOptions: [],
     sourceFiles: ["src/language/JSONSchema/JSONSchemaRenderer.ts"],
