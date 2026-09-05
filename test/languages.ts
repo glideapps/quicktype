7fee4c7a2e205d5e2070bfe0a3533b6d2d52b2fa
Mark Probst <mark.probst@gmail.com>
2026-09-05 12:41:06 -0700

    Cover Dart runtime names in shared keyword input

--- test/languages.ts
+++ test/languages.ts
@@ -1515,7 +1515,6 @@ export const DartLanguage: Language = {
     skipJSON: [],
     additionalSchemaFiles: [
         "test/inputs/regressions/unicode-codepoint-length.schema",
-        "test/inputs/dart/dart-runtime-type-names.schema",
     ],
     skipSchema: [
         "optional-property.schema",
