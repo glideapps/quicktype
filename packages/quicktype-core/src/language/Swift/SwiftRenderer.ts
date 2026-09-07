b848720ff991a5e25d8f194da6d4aed99b478368
Mark Probst <mark.probst@gmail.com>
2026-09-07 13:40:20 -0400

    fix(swift): emit requested coding keys protocol (#3502)

--- packages/quicktype-core/src/language/Swift/SwiftRenderer.ts
+++ packages/quicktype-core/src/language/Swift/SwiftRenderer.ts
@@ -627,7 +627,11 @@ export class SwiftRenderer extends ConvenienceRenderer {
                     const allPropertiesRedundant = groups.every((group) => {
                         return group.every((p) => p.label === undefined);
                     });
-                    if (!allPropertiesRedundant && c.getProperties().size > 0) {
+                    if (
+                        (!allPropertiesRedundant ||
+                            this._options.codingKeysProtocol.length > 0) &&
+                        c.getProperties().size > 0
+                    ) {
                         this.ensureBlankLine();
                         let enumDeclaration = this.accessLevel;
                         enumDeclaration += "enum CodingKeys: String, CodingKey";
